9bcc115886578270d1816a993cec29523ac57119
Alex Zaharia <alecszaharia@gmail.com>
2018-04-24 17:02:55 +0300

    Return the post on save trigger api call.

--- editor/api.php
+++ editor/api.php
@@ -98,8 +98,11 @@ class Brizy_Editor_API {
 			wp_publish_post( $post_id );
 			add_action( 'save_post', array( $this, 'compile_post_action' ), 10, 2 );
 
-			//wp_update_post($wp_post);
-			//wp_publish_post( $post_id );
+			$post_arr = self::create_post_arr( $post );
+
+			$post_arr['is_index'] = true; // this is for the case when the page we return is not an index page.. but the editor wants one.
+
+			$this->success( array( $post_arr ) );
 
 		} catch ( Exception $exception ) {
 			Brizy_Logger::instance()->exception($exception);
