56da01778339d10df0bbd0a62ae810987c706f7c
Nicolai Slovineanu <rockalife@gmail.com>
2018-05-15 16:20:14 +0300

    fixes in editor

--- brizy.php
+++ brizy.php
@@ -13,7 +13,7 @@
 
 define( 'BRIZY_DEVELOPMENT', false );
 define( 'BRIZY_VERSION', '1.0.2' );
-define( 'BRIZY_EDITOR_VERSION', '1.0.16' );
+define( 'BRIZY_EDITOR_VERSION', '1.0.17' );
 define( 'BRIZY_FILE', __FILE__ );
 define( 'BRIZY_PLUGIN_BASE', plugin_basename( BRIZY_FILE ) );
 define( 'BRIZY_PLUGIN_PATH', dirname( BRIZY_FILE ) );
