7da4b786e3be18f93df4f3f2003fd83aab893604
Marcel Lefter <marcellefter@outlook.com>
2018-05-21 15:29:39 +0300

    Add retina for brizy logo

--- admin/views/editor.php
+++ admin/views/editor.php
@@ -19,5 +19,5 @@ $plugins_url = plugins_url();
 
 <div class="brizy-editor">
     <a class="preview brizy-button brizy-button--primary" type="button" href="<?php echo $url; ?>"><?php _e( 'Continue to edit with', 'brizy' ); ?>&nbsp;
-        <img src="<?php echo plugins_url( '../static/img/brizy.png', __FILE__ );?>" class="brizy-logo" /></a>
+        <img src="<?php echo plugins_url( '../static/img/brizy.png', __FILE__ );?>" srcset="<?php echo plugins_url( '../static/img/brizy.png', __FILE__ );?> 1x, <?php echo plugins_url( '../static/img/brizy-2x.png', __FILE__ );?> 2x" class="brizy-logo" /></a>
 </div>
