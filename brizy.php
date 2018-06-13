408ec7be917347c2ec02512df9a5ed6ae96403df
Alex Zaharia <alecszaharia@gmail.com>
2018-06-13 11:25:56 +0300

    Fixed the function that returns the current post id

--- brizy.php
+++ brizy.php
@@ -14,7 +14,7 @@
 define( 'BRIZY_DEVELOPMENT', false );
 define( 'BRIZY_LOG', false );
 define( 'BRIZY_VERSION', '1.0.11' );
-define( 'BRIZY_EDITOR_VERSION', '1.0.33' );
+define( 'BRIZY_EDITOR_VERSION', '1.0.34' );
 define( 'BRIZY_FILE', __FILE__ );
 define( 'BRIZY_PLUGIN_BASE', plugin_basename( BRIZY_FILE ) );
 define( 'BRIZY_PLUGIN_PATH', dirname( BRIZY_FILE ) );
