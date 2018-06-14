8daec628e5db84a69942b276d11bd41a0eb60eae
Alex Zaharia <alecszaharia@gmail.com>
2018-06-14 17:12:10 +0300

    Fixed: update global values request

--- editor/api.php
+++ editor/api.php
@@ -395,8 +395,7 @@ class Brizy_Editor_API {
 			$posts = Brizy_Editor_Post::get_all_brizy_posts();
 
 			// we need to trigger a post update action to make sure the cache plugins will update clear the cache
-			wp_update_post( array( 'ID' => $post_id ) );
-
+			remove_action( 'save_post', array( Brizy_Admin_Main::_init(), 'compile_post_action' ) );
 			// mark all post to be compiled on next view
 			foreach ( $posts as $bpost ) {
 				$bpost->set_needs_compile( true );
