efca10494f608c04bcbf28122d188753b1b98f6e
Alex Zaharia <alecszaharia@gmail.com>
2018-06-25 06:59:57 -0400

    Build 1.0.16

--- brizy.php
+++ brizy.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://brizy.io/
  * Author: Brizy.io
  * Author URI: https://brizy.io/
- * Version: 1.0.15
+ * Version: 1.0.16
  * Text Domain: brizy
  * License: GPLv3
  * Domain Path: /languages
@@ -13,7 +13,7 @@
 
 define( 'BRIZY_DEVELOPMENT', false );
 define( 'BRIZY_LOG', false );
-define( 'BRIZY_VERSION', '1.0.15' );
+define( 'BRIZY_VERSION', '1.0.16' );
 define( 'BRIZY_EDITOR_VERSION', '1.0.39' );
 define( 'BRIZY_FILE', __FILE__ );
 define( 'BRIZY_PLUGIN_BASE', plugin_basename( BRIZY_FILE ) );
