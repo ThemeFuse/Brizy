3d20cd4b7439ccf449d09fd1441851d74a26be8e
Marcel Lefter <marcellefter@outlook.com>
2018-08-01 17:17:45 +0300

    Add language_attributes to brizy blank template

--- public/views/templates/brizy-blank-template.php
+++ public/views/templates/brizy-blank-template.php
@@ -1,19 +1,20 @@
 <?php
+
 /**
  * Template Name: Blank Template
  */
 ?>
-<?php if ( ! defined( 'ABSPATH' ) ) {
-	die( 'Direct access forbidden.' );
+<?php if (!defined('ABSPATH')) {
+	die('Direct access forbidden.');
 }
 ?>
 <!DOCTYPE html>
-<html>
+<html <?php language_attributes(); ?>>
 <head>
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-<?php while ( have_posts() ) : the_post() ?>
+<?php while (have_posts()) : the_post() ?>
 	<?php the_content() ?>
 <?php endwhile ?>
 <?php wp_footer(); ?>
