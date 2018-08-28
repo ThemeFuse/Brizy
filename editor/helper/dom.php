3d6fdb5e001760e6e50044d26b58733d5c5aff39
Viorel <eremiaviorel0@gamil.com>
2018-08-28 17:56:32 +0300

    Fixed ThemeFuse/Brizy/#515

--- editor/helper/dom.php
+++ editor/helper/dom.php
@@ -6,7 +6,7 @@ class Brizy_Editor_Helper_Dom extends Brizy_Editor_Helper_DomTag {
 	 * @return Brizy_Editor_Helper_DomTag
 	 */
 	public function get_head() {
-		$tags = $this->get_tags( '/(<head(.*?)<\/head>)/is' );
+		$tags = $this->get_tags( '/(<body(.*)<\/body>)/mis' );
 
 		if ( empty( $tags ) ) {
 			return new Brizy_Editor_Helper_DomTag('');
