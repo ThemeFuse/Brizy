a0a5630ed117b8e3bacd4d6e692b409f8a6ccf2d
Alex Zaharia <alecszaharia@gmail.com>
2018-09-24 11:17:29 +0300

    Try no to run migration for new installs

--- admin/migrations/null-migration.php
+++ admin/migrations/null-migration.php
@@ -14,6 +14,17 @@ class Brizy_Admin_Migrations_NullMigration implements Brizy_Admin_Migrations_Mig
 	 * @return mixed
 	 */
 	public function getVersion() {
+
+		// check if the DB has traces of brizy plugin..
+		// if yes then return version 0.0.1 to run all migrations
+		// if no then return the current brizy version and no migration will be run
+
+		$option = get_option( Brizy_Editor_Storage_Common::KEY, null );
+
+		if ( is_null( $option ) ) {
+			return BRIZY_VERSION;
+		}
+
 		return '0.0.1';
 	}
 
