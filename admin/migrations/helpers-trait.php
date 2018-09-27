2090ac52fd361141a7e3729cabdb3e3187b9c878
Vlad <andronicvlad90@gmail.com>
2018-09-27 10:23:15 +0300

    add common functions in helpers trait

--- admin/migrations/helpers-trait.php
+++ admin/migrations/helpers-trait.php
@@ -16,6 +16,63 @@ trait Brizy_Admin_Migrations_HelpersTrait {
 		return $array;
 	}
 
+	/**
+	 * Get posts and meta
+	 */
+	public function get_posts_and_meta() {
+		global $wpdb;
+
+		// query all posts (all post_type, all post_status) that have meta_key = 'brizy' and is not 'revision'
+		return $wpdb->get_results("
+			SELECT p.ID, pm.meta_value FROM {$wpdb->postmeta} pm
+			LEFT JOIN {$wpdb->posts} p ON p.ID = pm.post_id
+			WHERE pm.meta_key = 'brizy'
+			AND p.post_type != 'revision'
+			AND p.post_type != 'attachment'
+		");
+	}
+
+	/**
+	 * Migrate post
+	 */
+	public function migrate_post($json_value, $post_id) {
+		$old_arr = json_decode($json_value, true);
+
+		$debug = true;
+		$debug = false;
+		if ( $debug ) {
+			// write in before.json to track the changes
+			$result_old = file_put_contents($post_id.'-before.json', json_encode(
+				$old_arr,
+				JSON_UNESCAPED_SLASHES|JSON_PRETTY_PRINT
+			));
+			echo 'put-contents-before-json=='.$result_old.'<br>';
+		}
+
+		// todo: need here to inspect if is allow inline function in PHP 5.4
+		$new_arr = $this->array_walk_recursive_and_delete($old_arr, function ($value, $key) {
+			if ( is_array($value) ) {
+				return empty($value);
+			}
+
+			if ( isset($value['type']) && isset($value['value']) ) {
+				// if is shortcode return true
+				return true;
+			}
+		});
+
+		if ( $debug ) {
+			// write in before.json to track the changes
+			$result_new = file_put_contents($post_id.'-after.json', json_encode(
+				$new_arr,
+				JSON_UNESCAPED_SLASHES|JSON_PRETTY_PRINT
+			));
+			echo 'put-contents-before-json=='.$result_new.'<br>';
+		}
+
+		return json_encode($new_arr);
+	}
+
 	public function parse_shortcodes(array &$array) {
 		// rewrite this function in your class
 
