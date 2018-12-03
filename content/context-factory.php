f902bdcfda7ae743f564a55bd5894985385dcf62
Viorel <eremiaviorel0@gamil.com>
2018-12-03 11:53:31 +0200

    Issue #907 - add new filter for loop context

--- content/context-factory.php
+++ content/context-factory.php
@@ -17,15 +17,38 @@ class Brizy_Content_ContextFactory {
 	 * @return Brizy_Content_Context
 	 */
 	static public function createContext(  $project, $brizy_post, $wp_post, $contentHtml ) {
+		$context = self::getContext( $project, $wp_post );
+
+		return apply_filters( 'brizy_context_create', $context, $wp_post );
+	}
+
+	/**
+	 * @param $project
+	 * @param $brizy_post
+	 * @param $wp_post
+	 * @param $contentHtml
+	 *
+	 * @return Brizy_Content_Context
+	 */
+	static public function createLoopContext(  $project, $brizy_post, $wp_post, $contentHtml ) {
+		$context = self::getContext( $project, $wp_post );
+
+		return apply_filters( 'brizy_loop_context_create', $context, $wp_post );
+	}
+
+	/**
+	 * @param $project
+	 * @param $wp_post
+	 *
+	 * @return Brizy_Content_Context
+	 */
+	private static function getContext( $project, $wp_post ) {
 		$context = new Brizy_Content_Context( $project, null, $wp_post,null );
 
 		if ( $wp_post ) {
 			$context->setAuthor( $wp_post->post_author );
 		}
 
-		/**
-		 * We send here the $wp_post for compatibility
-		 */
-		return apply_filters( 'brizy_context_create', $context, $wp_post );
+		return $context;
 	}
 }
