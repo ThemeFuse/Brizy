d83abaf505a86bb6ee56a0b3eefc6e7aa09502d2
Viorel <eremiaviorel0@gmail.com>
2019-02-19 12:50:31 +0200

    Fix undefined class

--- compatibilities/wordpress-mu-domain-mapping.php
+++ compatibilities/wordpress-mu-domain-mapping.php
@@ -2,7 +2,7 @@
 /**
  * Compatibility with Fast Velocity Minify plugin: https://wordpress.org/plugins/wordpress-mu-domain-mapping/
  */
-class Brizy_Compatibilities_WordpressMuDmainMapping {
+class Brizy_Compatibilities_WordpressMuDomainMapping {
 
 	public function __construct() {
 		add_action( 'template_redirect', array( $this, 'remove_redirect_to_mapped_domain' ), 9 );
