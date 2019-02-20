d159ae6127deb9a07f19ea40fb5c4adeff8f1974
Alex Zaharia <alecszaharia@gmail.com>
2019-02-20 14:48:48 +0200

    Added wp language placeholder
    Added unique page url class

---
+++ content/placeholders/unique-page-url.php
@@ -0,0 +1,65 @@
+<?php
+
+class Brizy_Content_Placeholders_UniquePageUrl extends Brizy_Content_Placeholders_Simple {
+
+
+	/**
+	 * Brizy_Content_Placeholders_Simple constructor.
+	 *
+	 * @param $label
+	 * @param $placeholder
+	 * @param $value
+	 * @param string $display
+	 */
+	public function __construct( $label, $placeholder = 'brizy_dc_current_page_unique_url', $value = null, $display = Brizy_Content_Placeholders_Abstract::DISPLAY_INLINE ) {
+		$this->setLabel( $label );
+		$this->setPlaceholder( $placeholder );
+		$this->setDisplay( $display );
+
+		$this->value = null;
+	}
+
+	/**
+	 * @param Brizy_Content_ContentPlaceholder $contentPlaceholder
+	 * @param Brizy_Content_Context $context
+	 *
+	 * @return mixed|string
+	 */
+	public function getValue( Brizy_Content_Context $context, Brizy_Content_ContentPlaceholder $contentPlaceholder ) {
+
+		global $wp;
+
+		$url = home_url( add_query_arg(array(), $wp->request) );
+
+		$closure = function () {
+			return false;
+		};
+
+		add_filter( 'pre_term_link', $closure );
+
+		$object = get_queried_object();
+
+		if(is_archive()) {
+			$url =  add_query_arg($wp->query_vars, home_url() );
+		}
+
+		if ( $object instanceof WP_User ) {
+			$file = home_url( '/' );
+			$url = $file . '?author=' . $object->ID;
+		}
+
+		if ( $object instanceof WP_Post ) {
+			$url = $object->guid;
+		}
+
+		if ( $object instanceof WP_Term ) {
+			$url = get_term_link( $object );
+		}
+
+		remove_filter( 'pre_term_link', $closure );
+
+		return $url;
+	}
+
+
+}
--- content/providers/free-provider.php
+++ content/providers/free-provider.php
@@ -19,7 +19,7 @@ class Brizy_Content_Providers_FreeProvider extends Brizy_Content_Providers_Abstr
 		return array(
 			new Brizy_Content_Placeholders_ImageTitleAttribute( 'Internal Title Attributes', 'brizy_dc_image_title' ),
 			new Brizy_Content_Placeholders_ImageAltAttribute( 'Internal Alt Attributes', 'brizy_dc_image_alt' ),
-			new Brizy_Content_Placeholders_UniquePageUrl( 'Uniquer page url' ),
+			new Brizy_Content_Placeholders_UniquePageUrl( 'Uniquer page url','brizy_dc_current_page_unique_url' ),
 			new Brizy_Content_Placeholders_Simple( 'WP Language', 'brizy_dc_page_language', get_locale() )
 		);
 	}
