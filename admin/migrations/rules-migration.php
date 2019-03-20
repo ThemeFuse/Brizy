7f1fc90bdd94e0c86d48c32db24839c0c6b749c6
Alex Zaharia <alecszaharia@gmail.com>
2019-03-20 17:06:29 +0200

    Fixed rule migration

--- admin/migrations/rules-migration.php
+++ admin/migrations/rules-migration.php
@@ -20,11 +20,11 @@ class Brizy_Admin_Migrations_RulesMigration implements Brizy_Admin_Migrations_Mi
 
 		$templateIds = $this->getTemplateIds();
 
-		foreach($templateIds as $templateId) {
-			$data  = get_post_meta( (int) $templateId, 'brizy-template-rules', true );
-			add_post_meta( (int) $templateId, 'brizy-bk-'.get_class($this).'-'.$this->getVersion(), $data );
-			add_post_meta( (int) $templateId, 'brizy-rules', $data );
-			delete_post_meta( (int) $templateId, 'brizy-template-rules' );
+		foreach ( $templateIds as $templateId ) {
+			$data = get_post_meta( (int) $templateId->post_id, 'brizy-template-rules', true );
+			add_post_meta( (int) $templateId->post_id, 'brizy-bk-' . get_class( $this ) . '-' . $this->getVersion(), $data );
+			add_post_meta( (int) $templateId->post_id, 'brizy-rules', $data );
+			delete_post_meta( (int) $templateId->post_id, 'brizy-template-rules' );
 		}
 	}
 
@@ -35,9 +35,6 @@ class Brizy_Admin_Migrations_RulesMigration implements Brizy_Admin_Migrations_Mi
 		global $wpdb;
 
 		// query all posts (all post_type, all post_status) that have meta_key = 'brizy' and is not 'revision'
-		return $wpdb->get_results("
-			SELECT pm.post_id
-			FROM {$wpdb->postmeta} pm				
-			WHERE pm.meta_key = 'brizy-template-rules'");
+		return $wpdb->get_results( "SELECT pm.post_id FROM {$wpdb->postmeta} pm WHERE pm.meta_key = 'brizy-template-rules'" );
 	}
 }
