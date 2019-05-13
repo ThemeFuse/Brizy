01df348153e4de7538272b1a1a0946bea51b84ce
Viorel <eremiaviorel0@gmail.com>
2019-05-13 17:10:11 +0300

    News in the wp dashboard

--- admin/dashboard-widget.php
+++ admin/dashboard-widget.php
@@ -30,7 +30,7 @@ class Brizy_Admin_DashboardWidget extends Brizy_Admin_AbstractWidget {
 
 	public function render() {
 		echo Brizy_Admin_View::render( 'dashboard', array(
-			//'news'  => $this->renderNews(),
+			'news'  => $this->renderNews(),
 			'posts' => $this->renderBrizyPosts()
 		) );
 	}
@@ -39,24 +39,71 @@ class Brizy_Admin_DashboardWidget extends Brizy_Admin_AbstractWidget {
 	 * @return string
 	 */
 	private function renderNews() {
-		return Brizy_Admin_View::render( 'dashboard-news', array() );
+		$news = [
+			[
+				'url'     => 'https://www.brizy.io/blog/white-label/',
+				'badge'   => 'new',
+				'title'   => 'Brizy Pro 0.0.20: White Label',
+				'excerpt' => 'Hydrogen is HERE :P. Just kidding, but if you had a company named Hydrogen you’d love our new White Label feature we just added in Brizy PRO.',
+			],
+			[
+				'url'     => 'https://www.brizy.io/blog/keyboard-shortcuts/',
+				'badge'   => 'new',
+				'title'   => 'Keyboard Shortcuts are Here',
+				'excerpt' => 'Starting today you’ll be creating client websites with Brizy even faster if that’s possible. We’ve added a bunch of keyboard shortcuts that will improve the speed you work with Brizy and make your life much easier when it comes to copy-pasting styles, duplicating content and more.',
+			],
+			[
+				'url'     => 'https://www.brizy.io/blog/new-premium-layout-packs/',
+				'badge'   => 'new',
+				'title'   => 'Brizy Pro 0.0.19: 17 new premium layout packs ',
+				'excerpt' => 'In our continuous effort to make Brizy the tool of choice when it comes to building stunning websites, I’d glad to tell you that we’ve just added 17 new premium layout packs that will let you kick start client websites in minutes. That is another 99 layouts on top of the 56 we already have.',
+			],
+		];
+
+		return Brizy_Admin_View::render( 'dashboard-news', [ 'news' => $news ] );
 	}
 
 	/**
 	 * @return string
 	 */
 	private function renderBrizyPosts() {
+
 		$query = array(
-			'post_type'   => Brizy_Editor::get()->supported_post_types(),
-			'meta_query'  => 'brizy',
-			'post_status' => array( 'publish', 'pending', 'draft', 'auto-draft', 'future', 'private', 'inherit' ),
-			'order'=>'DESC',
-			'order_by'=>'ID'
+			'post_type'   => array_diff( Brizy_Editor::get()->supported_post_types(), [ 'brizy-global-block', 'brizy-saved-block', 'brizy-global-block' ] ),
+			'post_status' => [ 'publish', 'draft' ],
+			'meta_key'    => 'brizy',
+			'orderby'     => 'modified'
 		);
 
-		$posts = get_posts( $query );
+		$posts       = get_posts( $query );
+		$brizy_posts = [];
 
-		return Brizy_Admin_View::render( 'dashboard-posts', array( 'posts' => $posts ) );
+		foreach ( $posts as $apost ) {
+
+			try {
+
+				$brizy_post = Brizy_Editor_Post::get( $apost );
+
+				if ( ! $brizy_post->uses_editor() ) {
+					continue;
+				}
+
+				$brizy_posts[] = [
+					'edit_url' => add_query_arg( [ Brizy_Editor_Constants::EDIT_KEY => '' ], get_permalink( $apost ) ),
+					'title'    => get_the_title( $apost ),
+					'date'     => get_the_modified_date( '', $apost )
+				];
+
+				if ( 6 === count( $brizy_posts ) ) {
+					break;
+				}
+
+			} catch ( Exception $e ) {
+				continue;
+			}
+		}
+
+		return Brizy_Admin_View::render( 'dashboard-posts', array( 'posts' => $brizy_posts ) );
 	}
 
 }
