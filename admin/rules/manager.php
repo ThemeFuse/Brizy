b64a5bef3640ca80843491a62f76159af409de71
Alex Zaharia <alecszaharia@gmail.com>
2019-06-03 16:16:02 +0300

    Fixed rule manager and block uid

--- admin/rules/manager.php
+++ admin/rules/manager.php
@@ -197,7 +197,7 @@ class Brizy_Admin_Rules_Manager {
 			$arrayRules[] = $rule->convertToOptionValue();
 		}
 
-		update_metadata( get_post_type((int)$postId), (int) $postId, 'brizy-rules', $arrayRules  );
+		update_metadata( 'post', (int) $postId, 'brizy-rules', $arrayRules  );
 	}
 
 	/**
--- editor/block.php
+++ editor/block.php
@@ -40,6 +40,7 @@ class Brizy_Editor_Block extends Brizy_Editor_Post {
 	public static function get( $apost, $uid = null ) {
 
 		$wp_post_id = $apost;
+
 		if ( $apost instanceof WP_Post ) {
 			$wp_post_id = $apost->ID;
 		}
