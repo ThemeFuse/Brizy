984eb6935c0c4e3a3416c1b9f7b6d4cdbd4bc398
Alex Zaharia <alecszaharia@gmail.com>
2019-07-03 10:53:20 +0300

    Changes in block positions update

--- tests/functional/BlockApiCest.php
+++ tests/functional/BlockApiCest.php
@@ -456,7 +456,7 @@ class BlockApiCest {
 	public function updateGlobalBlockPositionsTest( FunctionalTester $I ) {
 
 
-		$I->sendPOST( '/wp-admin/admin-ajax.php?' . build_query( [ 'action' => Brizy_Admin_Blocks_Api::UPDATE_BLOCK_POSITIONS_ACTION ] ), json_encode(
+		$I->sendPOST( '/wp-admin/admin-ajax.php?' . build_query( [ 'action' => Brizy_Admin_Blocks_Api::UPDATE_BLOCK_POSITIONS_ACTION ] ),
 			[
 				'blocks'      => [
 					'gffbf00297b0b4e9ee27af32a7b79c3330' => [ 'top' => 10, 'bottom' => 20, 'align' => "left" ],
@@ -464,12 +464,12 @@ class BlockApiCest {
 				],
 				'is_autosave' => 0
 			]
-		) );
+		);
 		$I->seeResponseCodeIsSuccessful();
 		$I->dontSeePostInDatabase( [ 'post_type' => 'revision' ] );
 
 
-		$I->sendPOST( '/wp-admin/admin-ajax.php?' . build_query( [ 'action' => Brizy_Admin_Blocks_Api::UPDATE_BLOCK_POSITIONS_ACTION ] ), json_encode(
+		$I->sendPOST( '/wp-admin/admin-ajax.php?' . build_query( [ 'action' => Brizy_Admin_Blocks_Api::UPDATE_BLOCK_POSITIONS_ACTION ] ),
 			[
 				'blocks'      => [
 					'gffbf00297b0b4e9ee27af32a7b79c3330' => [ 'top' => 10, 'bottom' => 20, 'align' => "left" ],
@@ -477,7 +477,7 @@ class BlockApiCest {
 				],
 				'is_autosave' => 1
 			]
-		) );
+		);
 		$I->seeResponseCodeIsSuccessful();
 		$I->seePostInDatabase( [ 'post_type' => 'revision' ] );
 
