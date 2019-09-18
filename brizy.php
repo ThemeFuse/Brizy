89f76174864ac441ef4082a463fe033bf9dc2485
Jenkins Jenkinovici <alecszaharia@gmail.com>
2019-09-18 12:50:32 +0000

    Build 1.0.82

--- brizy.php
+++ brizy.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://brizy.io/
  * Author: Brizy.io
  * Author URI: https://brizy.io/
- * Version: 1.0.81
+ * Version: 1.0.82
  * Text Domain: brizy
  * License: GPLv3
  * Domain Path: /languages
@@ -17,10 +17,10 @@ if ( isset( $_SERVER['HTTP_X_FORWARDED_PROTO'] ) && stripos( $_SERVER['HTTP_X_FO
 	$_SERVER['HTTPS'] = 'on';
 }
 
-define( 'BRIZY_DEVELOPMENT', true );
+define( 'BRIZY_DEVELOPMENT', false );
 define( 'BRIZY_LOG', false );
 define( 'BRIZY_VERSION', '1.0.82' );
-define( 'BRIZY_EDITOR_VERSION', '1.0.101' );
+define( 'BRIZY_EDITOR_VERSION', '102' );
 define( 'BRIZY_FILE', __FILE__ );
 define( 'BRIZY_PLUGIN_BASE', plugin_basename( BRIZY_FILE ) );
 define( 'BRIZY_PLUGIN_PATH', dirname( BRIZY_FILE ) );
