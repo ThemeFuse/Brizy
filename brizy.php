af506a09e2849e19f91ba169c73027552f4259be
Alex Zaharia <alecszaharia@gmail.com>
2019-09-20 11:24:35 +0300

    Changed plugin and editor versions

--- brizy.php
+++ brizy.php
@@ -19,8 +19,8 @@ if ( isset( $_SERVER['HTTP_X_FORWARDED_PROTO'] ) && stripos( $_SERVER['HTTP_X_FO
 
 define( 'BRIZY_DEVELOPMENT', false );
 define( 'BRIZY_LOG', false );
-define( 'BRIZY_VERSION', '1.0.84' );
-define( 'BRIZY_EDITOR_VERSION', '104' );
+define( 'BRIZY_VERSION', '1.0.85' );
+define( 'BRIZY_EDITOR_VERSION', '105' );
 define( 'BRIZY_FILE', __FILE__ );
 define( 'BRIZY_PLUGIN_BASE', plugin_basename( BRIZY_FILE ) );
 define( 'BRIZY_PLUGIN_PATH', dirname( BRIZY_FILE ) );
