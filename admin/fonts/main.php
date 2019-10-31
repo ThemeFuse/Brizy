93dd6d682a57ec10cdde2dfc6d3cd42a222ec34a
Alex Zaharia <alecszaharia@gmail.com>
2019-10-31 16:59:42 +0200

    Allow svg images

--- admin/fonts/main.php
+++ admin/fonts/main.php
@@ -10,6 +10,7 @@
 class Brizy_Admin_Fonts_Main {
 
 	const CP_FONT = 'brizy-font';
+	const SVG_MIME = 'image/svg+xml';
 
 	/**
 	 * @return Brizy_Admin_Fonts_Main
@@ -31,6 +32,8 @@ class Brizy_Admin_Fonts_Main {
 		add_action( 'wp_loaded', array( $this, 'initializeActions' ) );
 		add_filter( 'upload_mimes', array( $this, 'addFOntTypes' ) );
 		add_filter( 'wp_check_filetype_and_ext', array( $this, 'wp_check_filetype_and_ext' ), 10, 5 );
+		add_filter( 'wp_prepare_attachment_for_js', [ $this, 'wp_prepare_attachment_for_js' ], 10, 3 );
+		add_filter( 'wp_handle_upload_prefilter', array( $this, 'wp_handle_upload_prefilter' ) );
 
 		$urlBuilder = new Brizy_Editor_UrlBuilder();
 		$handler    = new Brizy_Admin_Fonts_Handler( $urlBuilder, null );
@@ -49,20 +52,46 @@ class Brizy_Admin_Fonts_Main {
 		$mime_types['eot']   = 'application/vnd.ms-fontobject';
 		$mime_types['woff']  = 'application/x-font-woff';
 		$mime_types['woff2'] = 'application/x-font-woff2';
-		$mime_types['svg']   = 'image/svg+xml';
+		$mime_types['svg']   = self::SVG_MIME;
 
 		return $mime_types;
 	}
 
+	public function wp_handle_upload_prefilter( $file ) {
+		if ( self::SVG_MIME !== $file['type'] ) {
+			return $file;
+		}
+
+		$dirtySVG = file_get_contents( $file['tmp_name'] );
+
+		$sanitizer = new \enshrined\svgSanitize\Sanitizer();
+		$cleanSVG  = $sanitizer->sanitize( $dirtySVG );
+		file_put_contents( $file['tmp_name'], $cleanSVG );
+
+		return $file;
+	}
+
+	/**
+	 * @param $data
+	 * @param $file
+	 * @param $filename
+	 * @param $mimes
+	 * @param $real_mime
+	 *
+	 * @return array
+	 */
 	public function wp_check_filetype_and_ext( $data, $file, $filename, $mimes, $real_mime ) {
 
-		if ( is_null( $data['ext'] ) ) {
+		if ( ! $data['ext'] ) {
 
 			// Do basic extension validation and MIME mapping
 			$wp_filetype = wp_check_filetype( $filename, $mimes );
 			$ext         = $wp_filetype['ext'];
 			$type        = $wp_filetype['type'];
 
+			if ( $ext === 'svg' ) {
+				return array( 'ext' => $ext, 'type' => $type, 'proper_filename' => false );
+			}
 			if ( $ext === 'ttf' ) {
 				return array( 'ext' => $ext, 'type' => 'application/x-font-ttf', 'proper_filename' => false );
 			}
@@ -80,7 +109,50 @@ class Brizy_Admin_Fonts_Main {
 		return $data;
 	}
 
-	static public function registerCustomPosts() {
+	/**
+	 * @param $attachment_data
+	 * @param $attachment
+	 * @param $meta
+	 *
+	 * @return mixed
+	 */
+	public function wp_prepare_attachment_for_js( $attachment_data, $attachment, $meta ) {
+
+		if ( 'image' !== $attachment_data['type'] || 'svg+xml' !== $attachment_data['subtype'] ) {
+			return $attachment_data;
+		}
+
+		if ( ! class_exists( 'SimpleXMLElement' ) ) {
+			return $attachment_data;
+		}
+
+		$svg = file_get_contents( get_attached_file( $attachment->ID ) );
+
+		if ( ! $svg ) {
+			return $attachment_data;
+		}
+
+		try {
+			$svg    = new \SimpleXMLElement( $svg );
+			$width  = (int) $svg['width'];
+			$height = (int) $svg['height'];
+		} catch ( \Exception $e ) {
+			return $attachment_data;
+		}
+
+		$src         = $url = $attachment_data['url'];
+		$orientation = $height > $width ? 'portrait' : 'landscape';
+
+		// Media Gallery
+		$attachment_data['image']         = compact( 'src', 'width', 'height' );
+		$attachment_data['thumb']         = compact( 'src', 'width', 'height' );
+		$attachment_data['sizes']['full'] = compact( 'url', 'height', 'width', 'orientation' );
+
+		return $attachment_data;
+	}
+
+
+	public static function registerCustomPosts() {
 
 		$labels = array(
 			'name' => _x( 'Fonts', 'post type general name' ),
