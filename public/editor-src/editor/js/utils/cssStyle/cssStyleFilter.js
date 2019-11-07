7c72ef9f948baa9637227982fbe381bb15c4ca04
Maxim Valuta <maxval1991@gmail.com>
2019-11-07 15:05:51 +0000

    Updated editor (120)

--- public/editor-src/editor/js/utils/cssStyle/cssStyleFilter.js
+++ public/editor-src/editor/js/utils/cssStyle/cssStyleFilter.js
@@ -5,10 +5,35 @@ import {
   styleFilterContrast
 } from "visual/utils/style2";
 
-export function cssStyleFilter({ v, device, state }) {
-  const brightness = styleFilterBrightness({ v, device, state });
-  const hue = styleFilterHue({ v, device, state });
-  const saturation = styleFilterSaturation({ v, device, state });
-  const contrast = styleFilterContrast({ v, device, state });
-  return `filter:brightness(${brightness}%) hue-rotate(${hue}deg) saturate(${saturation}%) contrast(${contrast}%);`;
+// Functia asta nu e nevoie de ea. Codul din ea trebuei sa fie in cssStyleFilter
+// Ea a fost facuta doar ca sa rezolvam problema cu safari in elementul image care folosea glamour
+export function cssStyleFilterSuffixForGlamour({
+  v,
+  device,
+  state,
+  prefix = ""
+}) {
+  const brightness = styleFilterBrightness({ v, device, state, prefix });
+  const hue = styleFilterHue({ v, device, state, prefix });
+  const saturation = styleFilterSaturation({ v, device, state, prefix });
+  const contrast = styleFilterContrast({ v, device, state, prefix });
+
+  return brightness === 100 &&
+    hue === 0 &&
+    saturation === 100 &&
+    contrast === 100
+    ? ""
+    : `brightness(${brightness}%) hue-rotate(${hue}deg) saturate(${saturation}%) contrast(${contrast}%);`;
+}
+
+export function cssStyleFilter({ v, device, state, prefix = "" }) {
+  const filter = cssStyleFilterSuffixForGlamour({
+    v,
+    device,
+    state,
+    prefix
+  });
+
+  if (filter === "") return "";
+  else return `filter:${filter};`;
 }
