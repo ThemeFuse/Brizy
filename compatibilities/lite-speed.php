04adc5322e2f31a5a449900d814a3fba2d20711a
Viorel <eremiaviorel0@gmail.com>
2019-11-28 15:15:01 +0200

    Fix incompatibility with lite speed cache plugin

--- compatibilities/lite-speed.php
+++ compatibilities/lite-speed.php
@@ -1,24 +1,20 @@
 <?php
+
 /**
  * Compatibility with LiteSpeed plugin: https://wordpress.org/plugins/litespeed-cache/
  */
 class Brizy_Compatibilities_LiteSpeed {
 
 	public function __construct() {
-		add_action( 'litespeed_option_js_minify',  array( $this, 'disable_js_minify' ), 10, 1 );
-	}
-
-	/**
-	 * @param $is_minify
-	 *
-	 * @return bool
-	 */
-	public function disable_js_minify( $is_minify ) {
-
-		if (  isset( $_GET['brizy-edit'] ) || isset( $_GET['brizy-edit-iframe'] ) ) {
-			return false;
+		if ( isset( $_GET['brizy-edit'] ) || isset( $_GET['brizy-edit-iframe'] ) ) {
+			add_action( 'litespeed_option_html_minify', '__return_false' );
+			add_action( 'litespeed_option_js_minify', '__return_false' );
+			add_action( 'litespeed_option_js_inline_minify', '__return_false' );
+			add_action( 'litespeed_option_js_combine', '__return_false' );
+			add_action( 'litespeed_option_css_minify', '__return_false' );
+			add_action( 'litespeed_option_css_inline_minify', '__return_false' );
+			add_action( 'litespeed_option_css_combine', '__return_false' );
+			add_action( 'litespeed_option_js_http2', '__return_false' );
 		}
-
-		return $is_minify;
 	}
 }
