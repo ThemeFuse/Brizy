289311a348b54691daa6db17647ecbff71adc2b2
Alex <alecszaharia@gmail.com>
2020-02-12 21:03:01 +0200

    Restrict access to site settings page

--- admin/site-settings.php
+++ admin/site-settings.php
@@ -5,6 +5,11 @@
 require_once( '../../../../wp-load.php' );
 require_once( ABSPATH . 'wp-admin/includes/file.php' );
 
+if(!is_user_logged_in() || !current_user_can('administrator') ) {
+    wp_redirect(admin_url());
+    exit;
+}
+
 if ( class_exists( 'Brizy_Admin_SiteSettings_Main' ) ) {
 	$settings = new Brizy_Admin_SiteSettings_Main( 'admin/site-settings.php' );
 	$settings->run();
