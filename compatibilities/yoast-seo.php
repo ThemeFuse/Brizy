a34766c8da6bc2af750b0633a9e4630e23ad2ffd
Viorel <eremiaviorel0@gmail.com>
2020-02-28 16:07:00 +0200

    Compile page in the admin edit post when yoast is activated - 8054

--- compatibilities/yoast-seo.php
+++ compatibilities/yoast-seo.php
@@ -3,6 +3,7 @@
 class Brizy_Compatibilities_YoastSeo {
 
 	public function __construct() {
+		add_filter( 'admin_init', array( $this, 'compile_post' ) );
 		add_filter( 'wpseo_twitter_image', array( $this, 'wpseo_twitter_image' ) );
 	}
 
@@ -37,5 +38,31 @@ class Brizy_Compatibilities_YoastSeo {
 		}
 
 		return $img_url;
+	}
+
+	public function compile_post() {
+		global $pagenow;
+
+		if ( 'post.php' !== $pagenow ) {
+			return;
+		}
+
+		try {
+			$post = Brizy_Editor_Post::get( $_GET['post'] );
+
+			if ( ! $post->uses_editor() ) {
+				return;
+			}
+
+			$needs_compile = ! $post->isCompiledWithCurrentVersion() || $post->get_needs_compile();
+
+			if ( $needs_compile ) {
+				$post->compile_page();
+				$post->saveStorage();
+				$post->savePost();
+			}
+
+		} catch ( Exception $e ) {
+		}
 	}
 }
