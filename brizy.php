a5f8ae80673db16640a56361dd74127c509bf976
Jenkins Jenkinovici <alecszaharia@gmail.com>
2020-03-27 09:01:27 +0000

    Build 1.0.121

--- brizy.php
+++ brizy.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://brizy.io/
  * Author: Brizy.io
  * Author URI: https://brizy.io/
- * Version: 1.0.120
+ * Version: 1.0.121
  * Text Domain: brizy
  * License: GPLv3
  * Domain Path: /languages
@@ -19,7 +19,7 @@ if ( isset( $_SERVER['HTTP_X_FORWARDED_PROTO'] ) && stripos( $_SERVER['HTTP_X_FO
 
 define( 'BRIZY_DEVELOPMENT', false );
 define( 'BRIZY_LOG', false );
-define( 'BRIZY_VERSION', '1.0.120' );
+define( 'BRIZY_VERSION', '1.0.121' );
 define( 'BRIZY_EDITOR_VERSION', '135-wp' );
 define( 'BRIZY_FILE', __FILE__ );
 define( 'BRIZY_PLUGIN_BASE', plugin_basename( BRIZY_FILE ) );
