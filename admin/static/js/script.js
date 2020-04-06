93c775cc532e0f31884900243ce7f9e3a31435dd
Viorel <eremiaviorel0@gmail.com>
2020-04-06 16:26:33 +0300

    Hide Gutenberg of wp 5.4

--- admin/static/js/script.js
+++ admin/static/js/script.js
@@ -157,14 +157,19 @@ jQuery(document).ready(function ($) {
     var BrizyGutenberg = {
 
         insertBrizyBtn: function () {
-            $('#editor').find('.edit-post-header-toolbar').append($('#brizy-gutenberg-btn-switch-mode').html());
+            var guten = $( '#editor' ),
+                html = $( '#brizy-gutenberg-btn-middle' ).html();
 
-            var html = $('#brizy-gutenberg-btn-middle').html();
+            if ( ! guten ) {
+                return;
+            }
 
-            if(html)
-            {
-                $('#editor').find('.editor-block-list__layout>*').css('display', 'none');
-                $('#editor').find('.editor-block-list__layout').append(html);
+            guten.find( '.edit-post-header-toolbar' ).append( $( '#brizy-gutenberg-btn-switch-mode' ).html() );
+
+            if ( html ) {
+                guten.find( '.block-editor-block-list__layout' ).hide();
+                guten.find( '.block-editor-writing-flow__click-redirect' ).hide();
+                guten.find( '.edit-post-visual-editor .block-editor-writing-flow' ).append( html );
             }
         },
 
