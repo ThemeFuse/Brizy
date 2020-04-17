81ee52a09416db16e391d134f62625df0479fed3
Alex Zaharia <alecszaharia>
2020-04-17 14:11:25 +0300

    Fixed assets path in asset proxy

--- public/asset-proxy.php
+++ public/asset-proxy.php
@@ -9,7 +9,7 @@ class Brizy_Public_AssetProxy extends Brizy_Public_AbstractProxy {
 	 * @return string
 	 */
 	protected function get_endpoint_keys() {
-		return array( Brizy_Editor::prefix(), Brizy_Editor::prefix(self::ENDPOINT_POST) );
+		return array( Brizy_Editor::prefix(), Brizy_Editor::prefix( self::ENDPOINT_POST ) );
 	}
 
 	public function process_query() {
@@ -22,13 +22,13 @@ class Brizy_Public_AssetProxy extends Brizy_Public_AbstractProxy {
 			return;
 		}
 
-		if ( ! isset( $vars[ Brizy_Editor::prefix(self::ENDPOINT_POST) ] ) || ! is_numeric( $vars[ Brizy_Editor::prefix(self::ENDPOINT_POST )] ) ) {
+		if ( ! isset( $vars[ Brizy_Editor::prefix( self::ENDPOINT_POST ) ] ) || ! is_numeric( $vars[ Brizy_Editor::prefix( self::ENDPOINT_POST ) ] ) ) {
 			return;
 		}
 
 		session_write_close();
 
-		$brizyPost = Brizy_Editor_Post::get( (int) $vars[ Brizy_Editor::prefix(self::ENDPOINT_POST )] );
+		$brizyPost = Brizy_Editor_Post::get( (int) $vars[ Brizy_Editor::prefix( self::ENDPOINT_POST ) ] );
 
 		if ( $brizyPost->uses_editor() ) {
 			$this->urlBuilder->set_post_id( $brizyPost->getWpPostId() );
@@ -40,7 +40,7 @@ class Brizy_Public_AssetProxy extends Brizy_Public_AbstractProxy {
 		$asset_path = "/" . ltrim( $endpoint_value, "/" );
 		$asset_url  = $this->urlBuilder->external_asset_url( $asset_path );
 
-		$new_path = $this->urlBuilder->page_upload_path( "assets/icons/" . basename( $asset_path ) );
+		$new_path = $this->urlBuilder->page_upload_path( "/assets".str_replace( '/editor', '', $asset_path ) );
 
 		if ( ! file_exists( $new_path ) ) {
 			$store_result = $this->store_file( $asset_url, $new_path );
@@ -58,9 +58,9 @@ class Brizy_Public_AssetProxy extends Brizy_Public_AbstractProxy {
 			$content = file_get_contents( $new_path );
 
 			// send headers
-			$headers                   = array();
-			$headers['Content-Type']   = $this->get_mime( $new_path, 1 );
-			$headers['Cache-Control']  = 'max-age=600';
+			$headers                  = array();
+			$headers['Content-Type']  = $this->get_mime( $new_path, 1 );
+			$headers['Cache-Control'] = 'max-age=600';
 
 			foreach ( $headers as $key => $val ) {
 				if ( is_array( $val ) ) {
