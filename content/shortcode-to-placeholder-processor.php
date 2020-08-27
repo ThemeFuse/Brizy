746327f19fa2ea75f3fa6d50c0841b770c4f2e3c
Viorel <eremiaviorel0@gmail.com>
2020-08-27 21:01:10 +0300

    placeholder to ajax

--- content/shortcode-to-placeholder-processor.php
+++ content/shortcode-to-placeholder-processor.php
@@ -9,10 +9,16 @@ class Brizy_Content_ShortcodeToPlaceholderProcessor implements Brizy_Editor_Cont
      */
     public function process( $content, Brizy_Content_Context $context ) {
 
-        $regex = '@((\[)|(\{\{))(brizy_)(.*?)((\])|(\}\}))@';
+		// Transform [brizy_shortcode_name attr="val"] to {{editor_shortcode_name attr='val'}}
+        $regex = '@(\[)(brizy_)(.*?)(\])@';
+
         $content = preg_replace_callback( $regex, function ( $matches ) {
-            // use replacer [brizy, {{brizy because there can be some attributes with the word brizy in the value
-            return str_replace( [ '[brizy', '{{brizy', ']', 'editor_dc' ], [ '{{editor', '{{editor', '}}', 'editor' ], $matches[0] );
+        	$shortcode = $matches[0];
+        	$shortcode = str_replace( [ '[brizy', ']' ], [ '{{editor', '}}' ], $shortcode );
+        	$shortcode = str_replace( '"', "'", $shortcode );
+
+            return $shortcode;
+
         }, $content  );
 
         return $content;
