ec9642bcea9550bf0958512a39cb396c3edf2efd
Alex Zaharia <alecszaharia@gmail.com>
2020-09-04 09:06:53 +0300

    #11364 - Disable Cloud Sync feature when the white labels is enabled

--- editor/editor/editor.php
+++ editor/editor/editor.php
@@ -872,8 +872,14 @@ class Brizy_Editor_Editor_Editor
      */
     public function getCloudInfo()
     {
+	    // the cloud will be always initialized with the exception when the white label is enabled
+	    // we wil return isSyncAllowed =  false just in case
+	    if ( !(class_exists( 'BrizyPro_Admin_WhiteLabel' ) && BrizyPro_Admin_WhiteLabel::_init()->getEnabled()) ) {
+		    return   array(
+			    'isSyncAllowed' => false,
+		    );
+	    }
 
-        // we 'allow' sync if you are not
         $response = array(
             'isSyncAllowed' => true,
         );
