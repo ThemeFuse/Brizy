7343574f1953b1dea618546589abbe26820729b3
Jenkins Jancovici <Jenkins@jancovici.com>
2020-09-11 12:18:20 +0000

    Updated editor (154-wp)

--- brizy.php
+++ brizy.php
@@ -20,8 +20,8 @@ if ( isset( $_SERVER['HTTP_X_FORWARDED_PROTO'] ) && stripos( $_SERVER['HTTP_X_FO
 define( 'BRIZY_DEVELOPMENT', false );
 define( 'BRIZY_LOG', false );
 define( 'BRIZY_VERSION', '2.0.8' );
-define( 'BRIZY_EDITOR_VERSION', BRIZY_DEVELOPMENT ? 'dev' : '153-wp' );
-define( 'BRIZY_SYNC_VERSION', '153' );
+define( 'BRIZY_EDITOR_VERSION', BRIZY_DEVELOPMENT ? 'dev' : '154-wp' );
+define( 'BRIZY_SYNC_VERSION', '154' );
 define( 'BRIZY_FILE', __FILE__ );
 define( 'BRIZY_PLUGIN_BASE', plugin_basename( BRIZY_FILE ) );
 define( 'BRIZY_PLUGIN_PATH', dirname( BRIZY_FILE ) );
