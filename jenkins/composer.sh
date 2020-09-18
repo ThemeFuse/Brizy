f8d46581de55e57d96a729ffad409b52ef2c0ebb
Alex Zaharia <alecszaharia@gmail.com>
2020-09-18 14:27:08 +0300

    Refactoriung the Jenkins file

--- jenkins/composer.sh
+++ jenkins/composer.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
-GITHUB_TOKEN=$1
-
 echo -e "\Add github token in composer $1"
 echo -e "-----------------------------------------------------------------------------"
 
 # add the github token
-/usr/local/bin/composer config -g github-oauth.github.com $GITHUB_TOKEN
+/usr/local/bin/composer config -g github-oauth.github.com $1
 
 echo -e "\Clear composer cache"
 echo -e "-----------------------------------------------------------------------------"
