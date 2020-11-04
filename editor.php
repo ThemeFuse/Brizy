90df72c32e704e5df4aec17c4d556d93c3147a68
Viorel <eremiaviorel0@gmail.com>
2020-11-04 12:56:37 +0200

    Postpone stories to another update

--- editor.php
+++ editor.php
@@ -136,7 +136,7 @@ class Brizy_Editor {
 		Brizy_Admin_FormEntries::_init();
 		Brizy_Admin_Fonts_Main::_init();
 		Brizy_Admin_Blocks_Main::_init();
-        Brizy_Admin_Stories_Main::_init();
+        //Brizy_Admin_Stories_Main::_init();
 
 		if ( Brizy_Editor::is_user_allowed() ) {
 			Brizy_Admin_Svg_Main::_init();
