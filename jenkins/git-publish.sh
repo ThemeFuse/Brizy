fe6aa9914bf821279938a9d9a564fbce89183635
Alex Zaharia <alecszaharia@gmail.com>
2021-04-21 11:16:49 +0300

    feat: Updated the git-publish.sh to create the fixes branch

--- jenkins/git-publish.sh
+++ jenkins/git-publish.sh
@@ -3,6 +3,7 @@
 BUILD_VERSION=$1
 EDITOR_VERSION=$2
 RELEASE_BRANCH=$3
+FIXES_BRANCH="fixes-$BUILD_VERSION"
 
 echo -e "\nPublishing the release in GIT"
 echo -e "-----------------------------------------------------------------------------"
@@ -13,7 +14,6 @@ git commit -a -m "Build $BUILD_VERSION"
 echo -e "\nPublishing the release branch: $RELEASE_BRANCH"
 echo -e "-----------------------------------------------------------------------------"
 git push origin $RELEASE_BRANCH
-
 
 echo -e "\nMerge the $RELEASE_BRANCH in master"
 echo -e "-----------------------------------------------------------------------------"
@@ -26,16 +26,18 @@ echo -e "\Creating the release tag: $BUILD_VERSION"
 echo -e "-----------------------------------------------------------------------------"
 git tag $BUILD_VERSION
 
+echo -e "\Creating the fixes branch: fixes-$BUILD_VERSION"
+echo -e "-----------------------------------------------------------------------------"
+git checkout -b $FIXES_BRANCH
+
 echo -e "\nPublishing the master branch and tags"
 echo -e "-----------------------------------------------------------------------------"
 git push origin master --tags
+git push origin $FIXES_BRANCH
 
 echo -e "\nMerge the $RELEASE_BRANCH in develop"
 echo -e "-----------------------------------------------------------------------------"
 
 git checkout -t origin/develop
 git merge --no-ff -m "Merge [$RELEASE_BRANCH] in develop" $RELEASE_BRANCH
-
-echo -e "\nPublishing the develop branch "
-echo -e "-----------------------------------------------------------------------------"
 git push origin develop
