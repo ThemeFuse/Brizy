a71440a2616f3f3ab8b836a1e74875b887b55b13
Alex Zaharia <alecszaharia@gmail.com>
2021-06-28 10:52:24 +0300

    fix: Cast rule values to int on serialization

--- admin/rule.php
+++ admin/rule.php
@@ -304,7 +304,9 @@ class Brizy_Admin_Rule extends Brizy_Admin_Serializable implements Brizy_Admin_R
 	 * @return string[]
 	 */
 	public function getEntityValues() {
-		return $this->entityValues;
+        return array_map(function ($v) {
+            return (int)$v;
+        }, $this->entityValues);
 	}
 
 	/**
