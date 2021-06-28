0569a42c4453e1b9271ac3f8578c887eb1976a3b
Alex Zaharia <alecszaharia@gmail.com>
2021-06-28 10:06:11 +0300

    fix: Refactor post_type to postType ion editor config

--- editor/editor/editor.php
+++ editor/editor/editor.php
@@ -152,7 +152,7 @@ class Brizy_Editor_Editor_Editor
                 'pluginPrefix' => Brizy_Editor::prefix(),
                 'permalink' => get_permalink($wp_post_id),
                 'page' => $wp_post_id,
-                'post_type' => get_post_type($wp_post_id),
+                'postType' => get_post_type($wp_post_id),
                 'featuredImage' => $this->getThumbnailData($wp_post_id),
                 'pageAttachments' => array('images' => $this->get_page_attachments()),
                 'templates' => $this->post->get_templates(),
