33c89b52974fb044603d4a4c31d11b76eea0d20b
Zaharia Alexandru <alecszaharia@gmail.com>
2021-07-20 20:25:45 +0300

    fix: added higher priority to compatibiliy class loadding

--- compatibilities/brizy-pro-compatibility.php
+++ compatibilities/brizy-pro-compatibility.php
@@ -3,11 +3,12 @@
 class Brizy_Compatibilities_BrizyProCompatibility {
 
 	public function __construct() {
-		if ( version_compare( BRIZY_PRO_VERSION, '2.3.0' ) < 1 && version_compare( BRIZY_VERSION, '2.3.0' ) ) {
+		$hasProInstalledAndEnabled = defined('BRIZY_PRO_VERSION');
+		if($hasProInstalledAndEnabled && version_compare(BRIZY_MINIMUM_PRO_VERSION, BRIZY_PRO_VERSION)>0) {
 			$proMain = new BrizyPro_Main();
 			add_action( 'wp_loaded', [ $proMain, 'wordpressLoaded' ], 11 );
 			add_action( 'admin_notices', [ $this, 'brizypro_upgrade_required' ] );
-			add_action( 'brizy_allow_plugin_included', '__return_false' );
+            add_action( 'brizy_allow_plugin_included', '__return_false' );
 		}
 	}
 
@@ -20,7 +21,7 @@ class Brizy_Compatibilities_BrizyProCompatibility {
 			<div class="notice notice-error is-dismissible">
 				<p>
 					<b><?php echo strtoupper( __bt( 'brizy', 'Brizy' ) ) ?> PRO IS NOT RUNNING. </b><br>
-					Please update <?php echo __bt( 'brizy', 'Brizy' ) ?> PRO 2.3.0 or newer.
+					Please update <?php echo __bt( 'brizy', 'Brizy' ) ?> PRO to the latest version.
 				</p>
 			</div>
 		<?php
--- compatibilities/init.php
+++ compatibilities/init.php
@@ -4,7 +4,7 @@ class Brizy_Compatibilities_Init {
 
 	public function __construct() {
 		$this->load_compatibilites();
-		add_action( 'plugins_loaded', array( $this, 'action_plugins_loaded' ) );
+		add_action( 'plugins_loaded', array( $this, 'action_plugins_loaded' ), 9 );
 		add_action( 'after_setup_theme', [ $this, 'after_setup_theme' ] );
 	}
 
