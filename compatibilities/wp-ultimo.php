785936162852cae1b2c9b6b549a13e9a6ea9f4af
Viorel <eremiaviorel0@gmail.com>
2021-10-07 17:24:05 +0300

    Fix: #15581 - Wp Ultimo plugin fix edit brizy pages with remapping domain.
    [ skip ci ]

---
+++ compatibilities/wp-ultimo.php
@@ -0,0 +1,36 @@
+<?php
+
+class Brizy_Compatibilities_WpUltimo {
+
+	public function __construct() {
+		add_action( 'init', [ $this, 'init' ] );
+	}
+
+	public function init() {
+		if ( isset( $_GET[ Brizy_Editor::prefix( '-edit' ) ] ) || isset( $_GET[ Brizy_Editor::prefix( '-edit-iframe' ) ] ) ) {
+			add_filter( 'brizy_editor_config', [ $this, 'remappingAssetsUrl' ] );
+		}
+	}
+
+	public function remappingAssetsUrl( $config ) {
+
+		if ( ! class_exists( 'Mercator\Mapping' ) ) {
+			return $config;
+		}
+
+		$mapping = Mercator\Mapping::get_by_domain( $_SERVER['HTTP_HOST'] );
+
+		if ( empty( $mapping ) || ! $mapping->is_active() ) {
+			return $config;
+		}
+
+		$url_mapped = $mapping->get_domain();
+
+		$original_url = get_blog_option( $mapping->get_site_id(), 'siteurl' );
+		$original_url = trim( preg_replace( '#^https?://#', '', $original_url ), '/' );
+
+		$config['urls']['assets'] = str_replace( $original_url, $url_mapped, $config['urls']['assets'] );
+
+		return $config;
+	}
+}
