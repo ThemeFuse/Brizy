0a57f64978114f8d7f66fc132dfbc94cc0ec2e7d
Alex Zaharia <alecszaharia@gmail.com>
2021-11-09 15:58:24 +0200

    new: Added extension presence check at the start of the plugin

--- editor.php
+++ editor.php
@@ -77,6 +77,10 @@ class Brizy_Editor {
 	 */
 	private function __construct() {
 
+		if((is_multisite() && is_network_admin()) || is_admin()) {
+			Brizy_SystemChecks::run();
+		}
+
 		// make sure the project is created
 		// do not remove this.. we force the project creation here.
 		$project = Brizy_Editor_Project::get();
