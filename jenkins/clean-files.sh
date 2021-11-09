ef7648ea0989367eb7020d62da87f7e8661985e8
Alex Zaharia <alecszaharia@gmail.com>
2021-11-09 10:34:02 +0200

    fix: Delete junk files but avoid twig folder
    (cherry picked from commit 084fb76603350c74c0c1fb033e0b3070b51683d8)

--- jenkins/clean-files.sh
+++ jenkins/clean-files.sh
@@ -17,7 +17,10 @@ rm -rf vendor
 echo -e "\nDelete all dev files"
 echo -e "-----------------------------------------------------------------------------"
 echo -e "Clean vendor folder"
-(  find ./vendor -type d -iname "docs" &&
+(
+   find ./vendor -type d -name "twig" -prune -o -type d -name tests -print &&
+   find ./vendor -type d -name "twig" -prune -o -type d -name test -print &&
+   find ./vendor -type d -iname "docs" &&
    find ./vendor -type d -name "doc" &&
    find ./vendor -type d -name ".git" &&
    find ./vendor -name "*.md" &&
