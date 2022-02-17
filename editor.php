b0a3eef3706a1849f32511cb3923ba97d5af591b
Alex Zaharia <alecszaharia@gmail.com>
2022-02-17 09:41:04 +0200

    fix: made the filter placeholder content action public

--- editor.php
+++ editor.php
@@ -335,11 +335,12 @@ class Brizy_Editor
     private function loadEditorApi($post, $user)
     {
         try {
-            if (Brizy_Editor_User::is_user_allowed()) {
-                new Brizy_Editor_RestExtend();
-                new Brizy_Editor_API($post);
-                new Brizy_Editor_BlockScreenshotApi($post);
-                new Brizy_Editor_Filters_Api(  );
+            new Brizy_Editor_Filters_Api(  );
+
+			if ( Brizy_Editor_User::is_user_allowed() ) {
+				new Brizy_Editor_RestExtend();
+				new Brizy_Editor_API( $post );
+				new Brizy_Editor_BlockScreenshotApi( $post );
 				Brizy_Editor_Accounts_Api::_init();
 			}
 
