7c2fca902190b95a4cc369a41e5ef8022cf82899
Jenkins Jancovici <Jenkins@jancovici.com>
2022-04-21 12:56:31 +0000

    Updated editor (232-wp)

--- public/editor-build/232-wp/texts.php
+++ public/editor-build/232-wp/texts.php
@@ -1,6 +1,6 @@
 <?php
 
-class Brizy_Public_EditorBuild_231Wp_Texts {
+class Brizy_Public_EditorBuild_232Wp_Texts {
 	public static function get_editor_texts() {
 		return apply_filters('brizy_editor_config_texts', array(
 			" Display Conditions" => __(" Display Conditions", "brizy"),
