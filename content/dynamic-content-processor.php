541471abe18b13c51ecac4400d70e054f23618d5
Alex Zaharia <alecszaharia@gmail.com>
2022-05-18 16:49:04 +0300

    fix: added page placeholders in context

--- content/dynamic-content-processor.php
+++ content/dynamic-content-processor.php
@@ -18,6 +18,15 @@ class Brizy_Content_DynamicContentProcessor implements Brizy_Editor_Content_Proc
 
 		$context->setProvider( $placeholderProvider );
 
+        // load all page placeholders
+        $postContent = $this->getBrizyPostContent($context->getProject(), Brizy_Editor_Post::get($context->getWpPost()));
+        /**
+         * @var \BrizyPlaceholders\ContentPlaceholder[] $contentPlaceholders;
+         */
+        list( $contentPlaceholders, $placeholderInstances, $newPostContent ) = $extractor->extract( $postContent );
+        $context->setPlaceholders($contentPlaceholders);
+
+
 		list( $contentPlaceholders, $placeholderInstances, $content ) = $extractor->extract( $content );
 
 		$replacer = new Replacer( $placeholderProvider );
@@ -26,4 +35,20 @@ class Brizy_Content_DynamicContentProcessor implements Brizy_Editor_Content_Proc
 
 		return $content;
 	}
+
+
+    private function getBrizyPostContent(Brizy_Editor_Project $project,Brizy_Editor_Post $post) {
+        $project = Brizy_Editor_Project::get();
+
+        if (!$post->get_compiled_html()) {
+            $compiled_html_body = $post->get_compiled_html_body();
+            $content = Brizy_SiteUrlReplacer::restoreSiteUrl($compiled_html_body);
+            $post->set_needs_compile(true)->saveStorage();
+        } else {
+            $compiled_page = $post->get_compiled_page();
+            $content = $compiled_page->get_body();
+        }
+
+        return $content;
+    }
 }
