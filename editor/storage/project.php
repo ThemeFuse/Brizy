f0bb06110005aadd50b42b4aa848609162db498f
Viorel <eremiaviorel0@gmail.com>
2022-07-26 15:41:22 +0300

    Fix: #19245 - Fixed project data should exist case

--- editor/storage/project.php
+++ editor/storage/project.php
@@ -48,19 +48,54 @@ class Brizy_Editor_Storage_Project extends Brizy_Editor_Storage_Post {
 
 		foreach ( $metadata[ $this->key() ] as $data ) {
 
-			$fixedData = preg_replace( "/O:(\d+):\"Brizy_(.+?)\"/u", "C:$1:\"Brizy_$2\"", $data );
+			$project = $this->tryToFixSerializer( $data );
 
-			if ( $fixedData ) {
-				$unserializedData = maybe_unserialize( $fixedData );
-
-				if ( $unserializedData ) {
-
-					$storage = self::instance( $this->get_id() );
-					$storage->loadStorage( $unserializedData );
-
-					return $unserializedData;
-				}
+			if ( $project ) {
+				return $project;
 			}
+		}
+
+		return [];
+	}
+
+	/**
+	 * @throws Exception
+	 */
+	private function tryToFixSerializer( $data ) {
+		$project = $this->tryToUnserialize( preg_replace( "/O:(\d+):\"Brizy_(.+?)\"/u", "C:$1:\"Brizy_$2\"", $data ) );
+
+		if ( $project ) {
+			return $project;
+		}
+
+		$data = preg_replace_callback(
+			'/(?<=^|\{|;)s:(\d+):\"(.*?)\";(?=[asbdiO]\:\d|N;|\}|$)/s',
+			function ( $m ) {
+				return 's:' . strlen( $m[2] ) . ':"' . $m[2] . '";';
+			},
+			$data
+		);
+
+		return $this->tryToUnserialize( $data );
+	}
+
+	/**
+	 * @throws Exception
+	 */
+	private function tryToUnserialize( $strSerialized ) {
+
+		if ( ! $strSerialized ) {
+			return [];
+		}
+
+		$unserializedData = maybe_unserialize( $strSerialized );
+
+		if ( $unserializedData ) {
+			$storage = self::instance( $this->get_id() );
+
+			$storage->loadStorage( $unserializedData );
+
+			return $unserializedData;
 		}
 
 		return [];
