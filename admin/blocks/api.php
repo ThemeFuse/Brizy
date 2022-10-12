c6615b95ca8baf53fef52e9c2c16f05202565e7f
Alex Zaharia <alecszaharia@gmail.com>
2022-10-12 14:12:50 +0300

    fix: Added current page rules and return draft global blocks

--- admin/blocks/api.php
+++ admin/blocks/api.php
@@ -81,7 +81,7 @@ class Brizy_Admin_Blocks_Api extends Brizy_Admin_AbstractApi {
 	public function addPageGlobalBlocks( $config, $context ) {
 
 		$blockManager = new Brizy_Admin_Blocks_Manager( Brizy_Admin_Blocks_Main::CP_GLOBAL );
-		$blocks       = $blockManager->getEntities(  [ ] );
+		$blocks       = $blockManager->getEntities(  ['status'=>'any'] );
 
 		$ruleManager = new Brizy_Admin_Rules_Manager();
 		$ruleSets    = [];
