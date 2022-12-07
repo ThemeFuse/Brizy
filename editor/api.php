150a25d7a3b494624ba706b40c677d6efdb627a8
Alex Zaharia <alecszaharia@gmail.com>
2022-12-07 14:38:11 +0200

    fix: return a placeholder for link in searhc posts

--- editor/api.php
+++ editor/api.php
@@ -924,7 +924,7 @@ class Brizy_Editor_API extends Brizy_Admin_AbstractApi
             $out[] = [
                 'ID'    => $post->ID,
                 'title' => $post->post_title,
-                'permalink'=>get_permalink($post)
+                'permalink'=>"{{brizy_dc_permalink post_id=\"{$post->ID}\"}}"
             ];
         }
 
