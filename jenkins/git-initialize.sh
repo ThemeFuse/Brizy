cf21877a8529f733e1cb2bf172260ce024b2f11a
Alex Zaharia <alecszaharia@gmail.com>
2022-12-19 16:50:33 +0200

    fix: git initialize script

--- jenkins/git-initialize.sh
+++ jenkins/git-initialize.sh
@@ -4,13 +4,15 @@ set -e
 # Deatached head fix.
 echo -e "\nGiT Cleanup"
 echo -e "-----------------------------------------------------------------------------"
-git clean -fd
-git fetch origin
+
+git fetch;
+git checkout .;
+git clean -fd;
 
 echo -e "\nCheckout: $1"
 echo -e "-----------------------------------------------------------------------------"
 
 git checkout master
 git branch -d $1
-git checkout -t origin/$1
-git reset --hard origin/$1
+git checkout -t origin/$1;
+git reset --hard origin/$1;
