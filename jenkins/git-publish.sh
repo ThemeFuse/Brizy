b5d39ca2ce4d639909c75473e6eb28c9c2596537
Zaharia Alexandru <alecszaharia@gmail.com>
2023-06-23 19:00:31 +0300

    fix: changed the jenkins file to leave the repo clean

--- jenkins/git-publish.sh
+++ jenkins/git-publish.sh
@@ -7,25 +7,22 @@ RELEASE_BRANCH=$3
 echo -e "\nPublishing the release in GIT"
 echo -e "-----------------------------------------------------------------------------"
 git add ./public/editor-build/$EDITOR_VERSION
+git add ./public/editor-client/*
+git add ./readme.txt
+git add ./README.md
+git add ./brizy.php
 git commit -a -m "Build $BUILD_VERSION"
 echo -e "\nPublishing the release branch: $RELEASE_BRANCH"
 echo -e "-----------------------------------------------------------------------------"
 git push origin $RELEASE_BRANCH
 
-git checkout develop
-git reset --hard origin/develop
-git merge --no-ff -m "Merge [$RELEASE_BRANCH] in develop" $RELEASE_BRANCH
-git push origin develop
-
 echo -e "\nMerge the $RELEASE_BRANCH in master"
 echo -e "-----------------------------------------------------------------------------"
-
-
-git branch -d master
-git checkout -t origin/master
+git checkout master
 git reset --hard origin/master
 # shellcheck disable=SC2086
 git merge --no-ff -m "Merge [$RELEASE_BRANCH] in master" $RELEASE_BRANCH
+git push origin master
 
 echo -e "\Creating the release tag: $BUILD_VERSION"
 echo -e "-----------------------------------------------------------------------------"
@@ -35,6 +32,12 @@ git tag $BUILD_VERSION
 
 echo -e "\nPublishing the master branch and tags"
 echo -e "-----------------------------------------------------------------------------"
-git push origin master --tags
+git push origin  --tags
+
+git checkout develop
+git reset --hard origin/develop
+git merge --no-ff -m "Merge [$RELEASE_BRANCH] in develop" $RELEASE_BRANCH
+git push origin develop
+
 
 
