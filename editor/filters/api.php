55ef69d9d5c97dd63d1b2a6b81bf6e5a0f4264d3
Zaharia Alexandru <alecszaharia@gmail.com>
2023-08-01 13:15:10 +0300

    fix: added a new method in context to find by name and id

--- editor/filters/api.php
+++ editor/filters/api.php
@@ -1,82 +1,94 @@
 <?php
 
-class Brizy_Editor_Filters_Api extends Brizy_Admin_AbstractApi {
+class Brizy_Editor_Filters_Api extends Brizy_Admin_AbstractApi
+{
 
-	const nonce = 'brizy-api';
-	const AJAX_FILTER_PLACEHOLDERS_CONTENT = '_filter_placeholders_content';
+    const nonce = 'brizy-api';
+    const AJAX_FILTER_PLACEHOLDERS_CONTENT = '_filter_placeholders_content';
 
-	protected function initializeApiActions() {
-		$this->addNoPrivAjaxAction( self::AJAX_FILTER_PLACEHOLDERS_CONTENT, array( $this, 'filterPlaceholdersContent' ) );
-		$this->addAjaxAction( self::AJAX_FILTER_PLACEHOLDERS_CONTENT, array( $this, 'filterPlaceholdersContent' ) );
-	}
+    protected function initializeApiActions()
+    {
+        $this->addNoPrivAjaxAction(self::AJAX_FILTER_PLACEHOLDERS_CONTENT, array($this, 'filterPlaceholdersContent'));
+        $this->addAjaxAction(self::AJAX_FILTER_PLACEHOLDERS_CONTENT, array($this, 'filterPlaceholdersContent'));
+    }
 
-	protected function getRequestNonce() {
-		return $this->param( 'hash' );
-	}
+    protected function getRequestNonce()
+    {
+        return $this->param('hash');
+    }
 
-	public function filterPlaceholdersContent() {
+    public function filterPlaceholdersContent()
+    {
 
-		if(empty($postId = $this->param('post_id'))) {
-			$this->error(400, 'Please provide the post id');
-		}
+        if (empty($postId = $this->param('post_id'))) {
+            $this->error(400, 'Please provide the post id');
+        }
 
-		if(empty($placeholders = $this->param('placeholders'))) {
-			$this->error(400, 'Please provide the placeholders param');
-		}
+        if (empty($placeholders = $this->param('placeholders'))) {
+            $this->error(400, 'Please provide the placeholders param');
+        }
 
-		$placeholderContents = [];
-		$brizyPost = Brizy_Editor_Post::get($postId);
-		$postContent = $this->getBrizyPostContent(Brizy_Editor_Project::get(), $brizyPost);
+        $placeholderContents = [];
+        $brizyPost = Brizy_Editor_Post::get($postId);
+        $postContent = $this->getBrizyPostContent(Brizy_Editor_Project::get(), $brizyPost);
 
         $placeholderProvider = new Brizy_Content_PlaceholderProvider();
-		$context = new Brizy_Content_Context(Brizy_Editor_Project::get(),$brizyPost,$brizyPost->getWpPost(),'');
-		$context->setProvider( $placeholderProvider );
-		$extractor           = new \BrizyPlaceholders\Extractor( $placeholderProvider );
+        $context = new Brizy_Content_Context(Brizy_Editor_Project::get(), $brizyPost, $brizyPost->getWpPost(), '');
+        $context->setProvider($placeholderProvider);
+        $extractor = new \BrizyPlaceholders\Extractor($placeholderProvider);
 
-		/**
-		 * @var \BrizyPlaceholders\ContentPlaceholder[] $contentPlaceholders;
-		 */
-		list( $contentPlaceholders, $placeholderInstances, $newPostContent ) = $extractor->extract( $postContent );
-		$context->setPlaceholders($contentPlaceholders);
+        /**
+         * @var \BrizyPlaceholders\ContentPlaceholder[] $contentPlaceholders ;
+         */
+        list($contentPlaceholders, $placeholderInstances, $newPostContent) = $extractor->extract($postContent);
+        $context->setPlaceholders($contentPlaceholders);
 
-		foreach( $placeholders as $placeholderId) {
-			try {
-				/**
-				 * @var \BrizyPlaceholders\ContentPlaceholder $placeholder;
-				 */
-				$placeholder = $context->getPlaceholderById($placeholderId);
+        foreach ($placeholders as $placeholderId) {
+            try {
+                /**
+                 * @var \BrizyPlaceholders\ContentPlaceholder $placeholder ;
+                 */
+                $cleanId = str_replace(['-tag', '-pag'], '', $placeholderId);
+                if (strpos($placeholderId, "-tag") !== false) {
+                    $placeholder = $context->getPlaceholderByNameAndId("brizy_dc_post_loop_tags", $cleanId);
+                } elseif (strpos($placeholderId, "-pag") !== false) {
+                    $placeholder = $context->getPlaceholderByNameAndId("brizy_dc_post_loop_pagination", $cleanId);
+                } else {
+                    $placeholder = $context->getPlaceholderById($placeholderId);
+                }
 
-				if(!$placeholder) {
+                if (!$placeholder) {
                     $placeholderContents[$placeholderId] = '';
                     continue;
                 }
 
-				/**
-				 * @var Brizy_Content_Placeholders_Abstract $placeholderInstance;
-				 */
-				$placeholderInstance = $placeholderProvider->getPlaceholderSupportingName($placeholder->getName());
-				$placeholderContents[$placeholderId] = $placeholderInstance->getValue($context,$placeholder);
-			} catch (\Exception $e) {
-				$placeholderContents[$placeholderId] = null;
-			}
-		}
+                /**
+                 * @var Brizy_Content_Placeholders_Abstract $placeholderInstance ;
+                 */
+                $placeholderInstance = $placeholderProvider->getPlaceholderSupportingName($placeholder->getName());
+                $placeholderContents[$placeholderId] = $placeholderInstance->getValue($context, $placeholder);
+            } catch (\Exception $e) {
+                $placeholderContents[$placeholderId] = null;
+            }
+        }
 
 
-		$this->success(['placeholders'=>$placeholderContents]);
-	}
+        $this->success(['placeholders' => $placeholderContents]);
+    }
 
-	private function getBrizyPostContent(Brizy_Editor_Project $project,Brizy_Editor_Post $post) {
-		$project = Brizy_Editor_Project::get();
+    private function getBrizyPostContent(Brizy_Editor_Project $project, Brizy_Editor_Post $post)
+    {
+        $project = Brizy_Editor_Project::get();
 
-		if (!$post->get_compiled_html()) {
-			$compiled_html_body = $post->get_compiled_html_body();
-			$content = Brizy_SiteUrlReplacer::restoreSiteUrl($compiled_html_body);
-			$post->set_needs_compile(true)->saveStorage();
-		} else {
-			$compiled_page = $post->get_compiled_page();
-			$content = $compiled_page->get_body();
-		}
+        if (!$post->get_compiled_html()) {
+            $compiled_html_body = $post->get_compiled_html_body();
+            $content = Brizy_SiteUrlReplacer::restoreSiteUrl($compiled_html_body);
+            $post->set_needs_compile(true)->saveStorage();
+        } else {
+            $compiled_page = $post->get_compiled_page();
+            $content = $compiled_page->get_body();
+        }
 
-		return $content;
-	}
+        return $content;
+    }
 }
