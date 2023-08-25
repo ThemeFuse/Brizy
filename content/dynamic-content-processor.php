09f2e596ed7dc207ca9eda769947ef90d937c312
Zaharia Alexandru <alecszaharia@gmail.com>
2023-08-25 10:38:36 +0300

    new: added recursive placeholder replace until there are no placeholders..

--- content/dynamic-content-processor.php
+++ content/dynamic-content-processor.php
@@ -24,14 +24,6 @@ class Brizy_Content_DynamicContentProcessor implements Brizy_Editor_Content_Proc
 
 		$content = $replacer->replaceWithExtractedData( $contentPlaceholders, $placeholderInstances, $content, $context );
 
-		// try to see if there are still placeholders that were not replaces
-		list( $contentPlaceholders, $placeholderInstances, $content2 ) = $extractor->extract( $content );
-		$level = (int)$context->getLevel();
-		if ( count( $placeholderInstances ) !== 0 && $level < 5 ) {
-			$context->setLevel( $level + 1 );
-			$content = $this->process( $content, $context );
-		}
-
 		return $content;
 	}
 }
