74ef332d40ee934ac25bbe908993c6a42e4a8c39
Boronciuc Andrei <kingstoonesis@gmail.com>
2024-03-11 14:35:00 +0200

    move getPlaceholderData to api-client

--- public/editor-client/src/dynamicContent/index.ts
+++ public/editor-client/src/dynamicContent/index.ts
@@ -1,5 +1,7 @@
-import { getPlaceholders } from "../api";
+import { getPlaceholders, getPlaceholdersData } from "../api";
 import { DCHandler } from "../types/DynamicContent";
+import { Response } from "../types/Response";
+import { Dictionary } from "../types/utils";
 import { t } from "../utils/i18n";
 
 export const placeholders: DCHandler = async (res, rej, extraData) => {
@@ -11,3 +13,16 @@ export const placeholders: DCHandler = async (res, rej, extraData) => {
     rej(t("Failed to load dynamic content placeholders"));
   }
 };
+
+export async function placeholderData(
+  res: Response<Dictionary<string[]>>,
+  rej: Response<string>,
+  extra: { placeholders: Dictionary<string>; signal?: AbortSignal }
+) {
+  try {
+    const data = await getPlaceholdersData(extra);
+    res(data);
+  } catch (e) {
+    rej(t("Fetch placeholder data error: ") + e);
+  }
+}
