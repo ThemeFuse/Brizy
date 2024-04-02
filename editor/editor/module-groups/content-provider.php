ef9f91796638a7b5d0ae47ecb8e63a8099d9eeea
Zaharia Alexandru <alecszaharia@gmail.com>
2024-04-02 22:05:44 +0300

    new: Added Flipbox in leftSidebar
    https://github.com/bagrinsergiu/Brizy-Cloud/issues/4004

--- editor/editor/module-groups/content-provider.php
+++ editor/editor/module-groups/content-provider.php
@@ -26,7 +26,8 @@ class Brizy_Editor_Editor_ModuleGroups_ContentProvider implements Brizy_Editor_E
 				"Switcher",
 				"Table",
 				"Timeline",
-				"Login"
+				"Login",
+				"Flipbox"
 			], 400 ),
 		];
 	}
