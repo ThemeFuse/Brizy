e1d99c7a748e1118979932c367914d2eb759bcb4
Viorel <eremiaviorel0@gmail.com>
2024-05-08 17:50:16 +0300

    Sanitize attributes

--- editor/trait/sanitize.php
+++ editor/trait/sanitize.php
@@ -12,7 +12,10 @@ trait Brizy_Editor_Trait_Sanitize {
 		}
 
 		$dataDecoded = wp_kses_post_deep( $dataDecoded );
+		$data        = json_encode( $dataDecoded );
+		$data        = preg_replace( '/javascript:[^"]*"/', '"', $data );
+		$data        = preg_replace( '/\s(on(click|mouseover|keydown|keyup|change|submit|load|error|focus|blur|select|dblclick))[:=]["\'][^"\']*["\']/i', '', $data );
 
-		return json_encode( $dataDecoded );
+		return $data;
 	}
 }
