228cf6eb5e55ca0dd091fc55e99ee4b31e2c812f
Maxim Valuta <maxval1991@gmail.com>
2024-05-24 13:32:29 +0300

    fix(api): update global blocks lazy 60 items per request

--- public/editor-client/src/publish/index.ts
+++ public/editor-client/src/publish/index.ts
@@ -1,6 +1,17 @@
 import { updateGlobalBlocks, updatePage, updateProject } from "@/api";
+import { GlobalBlock } from "@/types/GlobalBlocks";
 import { Publish } from "@/types/Publish";
 import { t } from "@/utils/i18n";
+
+// Size of each batch
+const batchSize = 60;
+
+async function updateGlobalBlocksLazy(items: Array<GlobalBlock>) {
+  for (let i = 0; i < items.length; i += batchSize) {
+    const batch = items.slice(i, i + batchSize);
+    await updateGlobalBlocks(batch, { is_autosave: 0 });
+  }
+}
 
 export const publish: Publish = {
   async handler(res, rej, args) {
@@ -25,7 +36,7 @@ export const publish: Publish = {
 
     if (globalBlocks && globalBlocks.length > 0) {
       try {
-        await updateGlobalBlocks(globalBlocks, { is_autosave: 0 });
+        await updateGlobalBlocksLazy(globalBlocks);
       } catch (e) {
         errors.push(t("Failed to update global blocks"));
       }
