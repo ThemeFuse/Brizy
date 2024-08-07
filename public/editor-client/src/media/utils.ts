818aa81299ae5d4c9bf9f5b97b52ab299dfc5d90
Gunka Artur <atrurel101010@mail.ru>
2024-08-07 14:37:20 +0300

    Fix for image duplicate

--- public/editor-client/src/media/utils.ts
+++ public/editor-client/src/media/utils.ts
@@ -2,9 +2,8 @@ import { getImageUid } from "../api";
 import { AddImageData, ImageUpload } from "../types/Media";
 
 export const uploadImage = ({
-  id,
-  fileName
+  id
 }: ImageUpload): Promise<AddImageData> =>
-  getImageUid(String(id)).then(({ uid }) => ({ uid, fileName }));
+  getImageUid(String(id)).then(({ uid }) => ({ uid, fileName: "" }));
 
 export const internalAcceptedExtensions = ["webp"];
