303e2b9347ea59a7cf5776e63a431060d9f61046
Zaharia Alexandru <alecszaharia@gmail.com>
2025-01-09 14:31:37 +0200

    refactor(sanitize): enhance CSS sanitization logic
    Commented out outdated unfiltered_html check and added filters to expand allowed CSS properties and validate style attributes. Improved regex to handle potential security risks while maintaining flexibility in accepted styles.

--- editor/trait/sanitize.php
+++ editor/trait/sanitize.php
@@ -4,19 +4,28 @@ trait Brizy_Editor_Trait_Sanitize {
 
 	public function sanitizeJson( $data ) {
 
-		if ( current_user_can( 'unfiltered_html' ) ) {
-			return $data;
-		}
+//		if ( current_user_can( 'unfiltered_html' ) ) {
+//			return $data;
+//		}
 		if ( ! $dataDecoded = json_decode( $data, true ) ) {
 			return $data;
 		}
-
 		add_filter( 'safe_style_css', function ( $styles ) {
 			$styles[] = 'rgba';
+			$styles[] = 'rgb';
 			$styles[] = 'var';
+			$styles[] = 'color';
+			$styles[] = 'linear-gradient';
+			$styles[] = 'display';
 
 			return $styles;
 		} );
+
+		add_filter( 'safecss_filter_attr_allow_css', function ( $css, $value ) {
+			// we may need to add more methods here
+			$preg_match = preg_match( '/(rgba?|hsla?|calc|opacity|blur)\(.*?\)/i', $value );
+			return $preg_match;
+		} ,10,2);
 
 		$dataDecoded = wp_kses_post_deep( $dataDecoded );
 		//$dataDecoded = $this->escapeJsonValues( $dataDecoded );
@@ -24,6 +33,7 @@ trait Brizy_Editor_Trait_Sanitize {
 		$data = preg_replace( '/javascript:.*?"/', '"', $data );
 		$data = preg_replace( '/javascript%3A.*?%22/', '%22', $data );
 		$data = preg_replace( '/(on(click|mouseover|keydown|keyup|change|submit|load|error|focus|blur|select|dblclick))\s*[:=]\s*(\\\"|\\\')(.*?)(\3)/i', '', $data );
+		//remove_filter( 'safecss_filter_attr_allow_css', '__return_true' );
 
 		return $data;
 	}
