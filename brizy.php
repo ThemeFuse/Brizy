6ffcc0d2190cdce59a8097b4cb0b5f41a512999e
Zaharia Alexandru <alecszaharia@gmail.com>
2025-05-13 09:49:09 +0300

    fix: update minimum compiler version to 315-wp

--- brizy.php
+++ brizy.php
@@ -21,7 +21,7 @@ define( 'BRIZY_DEVELOPMENT', false );
 define( 'BRIZY_LOG', false );
 define( 'BRIZY_VERSION', '2.6.17' );
 define( 'BRIZY_MINIMUM_PRO_VERSION', '2.4.15' );
-define( 'BRIZY_MINIMUM_COMPILER_VERSION', '314-wp' );
+define( 'BRIZY_MINIMUM_COMPILER_VERSION', '315-wp' );
 define( 'BRIZY_RECOMPILE_TAG', 1746602990 );
 define( 'BRIZY_EDITOR_VERSION', BRIZY_DEVELOPMENT ? 'dev' : '315-wp' );
 define( 'BRIZY_SYNC_VERSION', '315' );
