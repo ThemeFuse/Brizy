c5df71a512bdc44258b82ee8bc71774bd2086da7
Nichita <nichita@brizy.io>
2025-09-08 10:30:38 +0300

    feat(AI): Enhance template generation and import options

--- admin/static/js/ai-core.js
+++ admin/static/js/ai-core.js
@@ -11,13 +11,13 @@ document.addEventListener('DOMContentLoaded', function () {
 
         const sessionId = getUrlParameter('sessionId');
         const page = getUrlParameter('page');
+        const content = getUrlParameter('content');
 
         if (page === 'starter-templates' && sessionId.trim() !== '') {
-            generateTemplate(sessionId);
+            generateTemplate(sessionId, content);
         } else if (openAiModalButton) {
             openAiModalButton.addEventListener('click', function (event) {
                 event.preventDefault();
-                this.disabled = true;
                 createSession(this);
             });
         }
@@ -25,31 +25,236 @@ document.addEventListener('DOMContentLoaded', function () {
 
     initializeHandler();
 
-    function loadingModal(titleText, descriptionText) {
+    function generateTemplate(sessionId, content) {
+        loadingModal(
+            Brizy_Admin_Data.l10n.aiGenerateTemplateTitle,
+            Brizy_Admin_Data.l10n.aiGenerateTemplateDesc,
+        );
+
+        const ajaxData = {
+            action: Brizy_Admin_Data.aiPrefix + Brizy_Admin_Data.aiActions.generateTemplate,
+            hash: Brizy_Admin_Data.aiNonce,
+            sessionId: sessionId
+        };
+
+        fetch(Brizy_Admin_Data.url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: new URLSearchParams(ajaxData)
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`${response.status}`);
+                }
+                return response.json();
+            })
+            .then(response => {
+                if (response && response.success) {
+                    switch (content) {
+                        case 'delete':
+                            importContentDelete(response.data);
+                            break;
+                        case 'keep':
+                            importContentKeep(response.data);
+                            break;
+                        default:
+                            errorModal();
+                            break;
+                    }
+                } else {
+                    errorModal();
+                }
+            })
+            .catch(error => {
+                errorModal();
+            });
+    }
+
+    function importContentDelete(responseData) {
+        loadingModal(
+            null,
+            null,
+        );
+
+        const ajaxData = {
+            action: Brizy_Admin_Data.aiPrefix + Brizy_Admin_Data.aiActions.importDelete,
+            hash: Brizy_Admin_Data.aiNonce,
+            body: JSON.stringify(responseData)
+        };
+
+        fetch(Brizy_Admin_Data.url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: new URLSearchParams(ajaxData)
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`${response.status}`);
+                }
+                return response.json();
+            })
+            .then(response => {
+                if (response && response.success) {
+                    if (response.data) {
+                        let editPageUrl = response.data.editPageUrl;
+                        successModal(
+                            null,
+                            null,
+                            null,
+                            editPageUrl,
+                        );
+                    } else {
+                        errorModal();
+                    }
+                } else {
+                    errorModal();
+                }
+            })
+            .catch(error => {
+                errorModal();
+            });
+    }
+
+    function importContentKeep(responseData) {
+        loadingModal(
+            null,
+            null,
+        );
+
+        const ajaxData = {
+            action: Brizy_Admin_Data.aiPrefix + Brizy_Admin_Data.aiActions.importKeep,
+            hash: Brizy_Admin_Data.aiNonce,
+            body: JSON.stringify(responseData)
+        };
+
+        fetch(Brizy_Admin_Data.url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: new URLSearchParams(ajaxData)
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`${response.status}`);
+                }
+                return response.json();
+            })
+            .then(response => {
+                if (response && response.success) {
+                    if (response.data) {
+                        let editPageUrl = response.data.editPageUrl;
+                        successModal(
+                            null,
+                            null,
+                            null,
+                            editPageUrl,
+                        );
+                    } else {
+                        errorModal();
+                    }
+                } else {
+                    errorModal();
+                }
+            })
+            .catch(error => {
+                errorModal();
+            });
+    }
+
+    function createSession(buttonElement) {
+        loadingModal(
+            Brizy_Admin_Data.l10n.aiCreatingSessionTitle,
+            Brizy_Admin_Data.l10n.aiCreatingSessionDesc,
+        );
+
+        const ajaxData = {
+            action: Brizy_Admin_Data.aiPrefix + Brizy_Admin_Data.aiActions.createSession,
+            hash: Brizy_Admin_Data.aiNonce
+        };
+
+        fetch(Brizy_Admin_Data.url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: new URLSearchParams(ajaxData)
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`${response.status}`);
+                }
+                return response.json();
+            })
+            .then(response => {
+                if (response && response.success) {
+                    typeGenerateModal(response);
+                } else {
+                    errorModal();
+                }
+            })
+            .catch(error => {
+                errorModal();
+            })
+            .finally(() => {
+                if (buttonElement) {
+                    buttonElement.disabled = false;
+                }
+            });
+    }
+
+    function typeGenerateModal(response) {
         const modalElement = document.querySelector('.brz-demo-modal');
         const modalContentContainer = document.querySelector('.brz-demo-modal-content');
-        const loadingTemplate = document.getElementById('brz-demo-modal-content-installing');
+        const installTemplate = document.getElementById('brz-demo-modal-content-install');
 
-        if (!modalElement || !modalContentContainer || !loadingTemplate) {
-            return;
-        }
+        if (modalContentContainer && installTemplate) {
+            modalContentContainer.innerHTML = installTemplate.innerHTML;
+            modalElement.classList.add('brz-demo-show-modal');
 
-        modalContentContainer.innerHTML = loadingTemplate.innerHTML;
-        modalElement.classList.add('brz-demo-show-modal');
+            const deleteBtn = modalContentContainer.querySelector('.js-demo-install[data-rm-content="1"]');
+            const keepBtn = modalContentContainer.querySelector('.js-demo-install[data-rm-content="0"]');
 
-        const modalTitle = modalContentContainer.querySelector('.brz-demo-modal-content-h3');
-        const modalDescription = modalContentContainer.querySelector('.brz-demo-modal-content-p');
+            if (deleteBtn) {
+                deleteBtn.dataset.response = JSON.stringify(response);
+                deleteBtn.addEventListener('click', handleDeleteClick);
+            }
 
-        if (titleText) {
-            if (modalTitle) {
-                modalTitle.textContent = titleText;
+            if (keepBtn) {
+                keepBtn.dataset.response = JSON.stringify(response);
+                keepBtn.addEventListener('click', handleKeepClick);
             }
         }
-        if (descriptionText) {
-            if (modalDescription) {
-                modalDescription.textContent = descriptionText;
-            }
-        }
+    }
+
+    function handleDeleteClick(event) {
+        event.preventDefault();
+        event.stopPropagation();
+
+        const buttonElement = event.currentTarget;
+        const responseString = buttonElement.dataset.response;
+        const responseData = JSON.parse(responseString);
+
+        let aiUrl = responseData.data.aiUrl + '/wp-admin/admin.php?page=starter-templates&content=delete';
+
+        window.location.href = aiUrl;
+    }
+
+    function handleKeepClick(event) {
+        event.preventDefault();
+        event.stopPropagation();
+
+        const buttonElement = event.currentTarget;
+        const responseString = buttonElement.dataset.response;
+        const responseData = JSON.parse(responseString);
+
+        let aiUrl = responseData.data.aiUrl + '/wp-admin/admin.php?page=starter-templates&content=keep';
+
+        window.location.href = aiUrl;
     }
 
     function successModal(titleText, descriptionText, buttonText, editUrl) {
@@ -91,7 +296,35 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
-    function showErrorModal() {
+    function loadingModal(titleText, descriptionText) {
+        const modalElement = document.querySelector('.brz-demo-modal');
+        const modalContentContainer = document.querySelector('.brz-demo-modal-content');
+        const loadingTemplate = document.getElementById('brz-demo-modal-content-installing');
+
+        if (!modalElement || !modalContentContainer || !loadingTemplate) {
+            return;
+        }
+
+        modalContentContainer.innerHTML = loadingTemplate.innerHTML;
+        modalElement.classList.add('brz-demo-show-modal');
+
+        const modalTitle = modalContentContainer.querySelector('.brz-demo-modal-content-h3');
+        const modalDescription = modalContentContainer.querySelector('.brz-demo-modal-content-p');
+
+        if (titleText) {
+            if (modalTitle) {
+                modalTitle.textContent = titleText;
+            }
+        }
+
+        if (descriptionText) {
+            if (modalDescription) {
+                modalDescription.textContent = descriptionText;
+            }
+        }
+    }
+
+    function errorModal() {
         const modalElement = document.querySelector('.brz-demo-modal');
         const errorContentContainer = document.querySelector('.brz-demo-modal-content');
         const errorTemplate = document.getElementById('brz-demo-modal-content-error');
@@ -102,227 +335,5 @@ document.addEventListener('DOMContentLoaded', function () {
 
         errorContentContainer.innerHTML = errorTemplate.innerHTML;
         modalElement.classList.add('brz-demo-show-modal');
-    }
-
-    function createSession(buttonElement) {
-        loadingModal(
-            Brizy_Admin_Data.l10n.aiCreatingSessionTitle,
-            Brizy_Admin_Data.l10n.aiCreatingSessionDesc,
-        );
-
-        const ajaxData = {
-            action: Brizy_Admin_Data.aiPrefix + Brizy_Admin_Data.aiActions.createSession,
-            hash: Brizy_Admin_Data.aiNonce
-        };
-
-        fetch(Brizy_Admin_Data.url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            body: new URLSearchParams(ajaxData)
-        })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error(`${response.status}`);
-                }
-                return response.json();
-            })
-            .then(response => {
-                if (response && response.success) {
-                    let aiUrl = response.data.aiUrl + '/wp-admin/admin.php?page=starter-templates';
-                    successModal(
-                        Brizy_Admin_Data.l10n.aiCreatingSessionSuccessTitle,
-                        Brizy_Admin_Data.l10n.aiCreatingSessionSuccessDesc,
-                        Brizy_Admin_Data.l10n.aiCreatingSessionSuccessBtn,
-                        aiUrl,
-                    );
-                } else {
-                    showErrorModal();
-                }
-            })
-            .catch(error => {
-                showErrorModal();
-            })
-            .finally(() => {
-                if (buttonElement) {
-                    buttonElement.disabled = false;
-                }
-            });
-    }
-
-    function generateTemplate(sessionId) {
-        loadingModal(
-            Brizy_Admin_Data.l10n.aiGenerateTemplateTitle,
-            Brizy_Admin_Data.l10n.aiGenerateTemplateDesc,
-        );
-
-        const ajaxData = {
-            action: Brizy_Admin_Data.aiPrefix + Brizy_Admin_Data.aiActions.generateTemplate,
-            hash: Brizy_Admin_Data.aiNonce,
-            sessionId: sessionId
-        };
-
-        fetch(Brizy_Admin_Data.url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            body: new URLSearchParams(ajaxData)
-        })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error(`${response.status}`);
-                }
-                return response.json();
-            })
-            .then(response => {
-                if (response && response.success) {
-                    typeImportModal(response);
-                } else {
-                    showErrorModal();
-                }
-            })
-            .catch(error => {
-                showErrorModal();
-            });
-    }
-
-    function typeImportModal(response) {
-        const modalElement = document.querySelector('.brz-demo-modal');
-        const modalContentContainer = document.querySelector('.brz-demo-modal-content');
-        const installTemplate = document.getElementById('brz-demo-modal-content-install');
-
-        if (modalContentContainer && installTemplate) {
-            modalContentContainer.innerHTML = installTemplate.innerHTML;
-            modalElement.classList.add('brz-demo-show-modal');
-
-            const deleteBtn = modalContentContainer.querySelector('.js-demo-install[data-rm-content="1"]');
-            const keepBtn = modalContentContainer.querySelector('.js-demo-install[data-rm-content="0"]');
-
-            if (deleteBtn) {
-                deleteBtn.dataset.response = JSON.stringify(response);
-                deleteBtn.addEventListener('click', handleDeleteClick);
-            }
-
-            if (keepBtn) {
-                keepBtn.dataset.response = JSON.stringify(response);
-                keepBtn.addEventListener('click', handleKeepClick);
-            }
-        }
-    }
-
-    function handleDeleteClick(event) {
-        event.preventDefault();
-        event.stopPropagation();
-
-        const buttonElement = event.currentTarget;
-        const responseString = buttonElement.dataset.response;
-        const responseData = JSON.parse(responseString);
-
-        loadingModal(
-            null,
-            null,
-        );
-
-        const ajaxData = {
-            action: Brizy_Admin_Data.aiPrefix + Brizy_Admin_Data.aiActions.importDelete,
-            hash: Brizy_Admin_Data.aiNonce,
-            body: JSON.stringify(responseData.data)
-        };
-
-        fetch(Brizy_Admin_Data.url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            body: new URLSearchParams(ajaxData)
-        })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error(`${response.status}`);
-                }
-                return response.json();
-            })
-            .then(response => {
-                if (response && response.success) {
-                    if (response.data) {
-                        let editPageUrl = response.data.editPageUrl;
-                        console.log(response);
-                        successModal(
-                            null,
-                            null,
-                            null,
-                            editPageUrl,
-                        );
-                    } else {
-                        console.log(response);
-                        showErrorModal();
-                    }
-                } else {
-                    showErrorModal();
-                }
-            })
-            .catch(error => {
-                showErrorModal();
-            });
-    }
-
-    function handleKeepClick(event) {
-        event.preventDefault();
-        event.stopPropagation();
-
-        const buttonElement = event.currentTarget;
-        const responseString = buttonElement.dataset.response;
-        const responseData = JSON.parse(responseString);
-
-        loadingModal(
-            null,
-            null,
-        );
-
-        const ajaxData = {
-            action: Brizy_Admin_Data.aiPrefix + Brizy_Admin_Data.aiActions.importKeep,
-            hash: Brizy_Admin_Data.aiNonce,
-            body: JSON.stringify(responseData.data)
-        };
-
-        fetch(Brizy_Admin_Data.url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            body: new URLSearchParams(ajaxData)
-        })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error(`${response.status}`);
-                }
-                return response.json();
-            })
-            .then(response => {
-                if (response && response.success) {
-                    if (response.data) {
-                        let editPageUrl = response.data.editPageUrl;
-                        console.log(response);
-                        successModal(
-                            null,
-                            null,
-                            null,
-                            editPageUrl,
-                        );
-                    } else {
-                        console.log(response);
-                        showErrorModal();
-                    }
-                } else {
-                    console.log(response);
-                    showErrorModal();
-                }
-            })
-            .catch(error => {
-                console.log(error);
-                showErrorModal();
-            });
     }
 });
