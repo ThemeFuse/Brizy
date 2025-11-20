60dd42ad192eae436f28a04ad5b08c4f01a88428
victor-pg <victor.poalelungi1@gmail.com>
2025-11-20 14:32:14 +0200

    remove state from api-client

--- public/editor-client/src/symbols/index.ts
+++ public/editor-client/src/symbols/index.ts
@@ -2,16 +2,16 @@ import {
   createSymbols,
   deleteSymbols,
   getSymbolsByUid,
-  getSymbolsList
+  getSymbolsList,
+  updateSymbols
 } from "@/api";
-import { symbolsState } from "@/state/symbols";
 import { CSSSymbol } from "@/types/Symbols";
 
 interface Symbols {
   getList: () => Promise<Array<CSSSymbol>>;
   getByUid: (uid: string[]) => Promise<CSSSymbol[]>;
-  create: (symbols: CSSSymbol[]) => void;
-  update: (symbol: CSSSymbol) => void;
+  create: (symbols: CSSSymbol[]) => Promise<CSSSymbol[]>;
+  update: (symbols: CSSSymbol[]) => void;
   remove: (symbols: CSSSymbol[]) => void;
 }
 
@@ -21,69 +21,39 @@ export const symbols: Symbols = {
   create: async (symbols: CSSSymbol[]) => {
     if (symbols.length > 0) {
       try {
+        console.log("symbols on create: ", symbols);
         const createdSymbols = await createSymbols(symbols);
 
-        if (createdSymbols.length > 0) {
-          symbolsState._published.push(...createdSymbols);
-        }
+        return createdSymbols;
       } catch (error) {
         if (process.env.NODE_ENV === "development") {
           console.error("Failed to create symbols:", error);
         }
       }
     }
-  },
-  update: async (symbol: CSSSymbol) => {
-    try {
-      const alreadyExists = symbolsState.current.find(
-        (s) => s.type === "update" && s.symbol.uid === symbol.uid
-      );
 
-      if (alreadyExists) {
-        symbolsState.current = symbolsState.current.map((s) =>
-          s.type === "update" && s.symbol.uid === symbol.uid
-            ? { type: "update", symbol }
-            : s
-        );
-      } else {
-        symbolsState.current.push({ type: "update", symbol });
-      }
-    } catch (error) {
-      if (process.env.NODE_ENV === "development") {
-        console.error("Failed to update symbol:", error);
+    return [];
+  },
+  update: async (symbols: CSSSymbol[]) => {
+    if (symbols.length > 0) {
+      try {
+        console.log("symbols on update: ", symbols);
+        await updateSymbols(symbols);
+      } catch (error) {
+        if (process.env.NODE_ENV === "development") {
+          console.error("Failed to update symbols:", error);
+        }
       }
     }
   },
   remove: async (symbols: CSSSymbol[]) => {
+    console.log("symbols on delete: ", symbols);
+
     const uids = symbols.map((symbol) => symbol.uid);
 
-    const idsToDelete: string[] = uids;
-
-    uids.forEach((uid) => {
-      const foundInCurrent = symbolsState.current.find(
-        (symbolPart) => symbolPart.symbol.uid === uid
-      );
-
-      if (foundInCurrent) {
-        symbolsState.current = symbolsState.current.filter(
-          (symbolPart) => symbolPart.symbol.uid !== uid
-        );
-      }
-
-      const foundInPublish = symbolsState._published.find(
-        (publishedSymbol) => publishedSymbol.uid === uid
-      );
-
-      if (foundInPublish) {
-        symbolsState._published = symbolsState._published.filter(
-          (symbol) => symbol.uid !== uid
-        );
-      }
-    });
-
-    if (idsToDelete.length > 0) {
+    if (uids.length > 0) {
       try {
-        await deleteSymbols(idsToDelete);
+        await deleteSymbols(uids);
       } catch (error) {
         if (process.env.NODE_ENV === "development") {
           console.error("Failed to delete symbols:", error);
