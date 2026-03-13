eadf2fd346234518f8a3fd25f4275d0d80bcbc01
Jenkins Jancovici <Jenkins@jancovici.com>
2026-03-13 07:31:00 +0000

    Build 2.8.2

--- brizy.php
+++ brizy.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://brizy.io/
  * Author: Brizy.io
  * Author URI: https://brizy.io/
- * Version: 2.8.1
+ * Version: 2.8.2
  * Text Domain: brizy
  * License: GPLv3
  * Domain Path: /languages
@@ -18,11 +18,11 @@ if ( isset( $_SERVER['HTTP_X_FORWARDED_PROTO'] ) && stripos( $_SERVER['HTTP_X_FO
 }
 define( 'BRIZY_DEVELOPMENT', false );
 define( 'BRIZY_LOG', false );
-define( 'BRIZY_VERSION', '2.8.1' );
+define( 'BRIZY_VERSION', '2.8.2' );
 define( 'BRIZY_MINIMUM_PRO_VERSION', '2.4.15' );
 define( 'BRIZY_MINIMUM_COMPILER_VERSION', '353-wp' );
-define( 'BRIZY_RECOMPILE_TAG', 1773231474 );
-define( 'BRIZY_EDITOR_VERSION', BRIZY_DEVELOPMENT ? 'dev' : '353-wp'  );
+define( 'BRIZY_RECOMPILE_TAG', 1773386907 );
+define( 'BRIZY_EDITOR_VERSION', BRIZY_DEVELOPMENT ? 'dev' : '353-wp'   );
 define( 'BRIZY_SYNC_VERSION', '353' );
 define( 'BRIZY_FILE', __FILE__ );
 define( 'BRIZY_PLUGIN_BASE', plugin_basename( BRIZY_FILE ) );
